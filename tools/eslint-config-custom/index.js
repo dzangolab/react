d98a17618bd27a8aba95448bcb71568551b9b389
Sudeep Bashistha <sudeep@dzangolab.com>
2022-12-12 11:23:04 +0545

    chore: added makefile and updated eslint config

--- tools/eslint-config-custom/index.js
+++ tools/eslint-config-custom/index.js
@@ -1,4 +1,74 @@
 module.exports = {
-  extends: [],
-  rules: {},
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "react-app",
+    "react-app/jest",
+    "prettier",
+  ],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["prettier", "unicorn"],
+  root: true,
+  rules: {
+    "import/order": [
+      1,
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling"],
+          "index",
+          "object",
+          "type",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    "prettier/prettier": "error",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          kebabCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "unicorn/numeric-separators-style": [
+      "error",
+      {
+        number: {
+          minimumDigits: 6,
+          groupLength: 3,
+        },
+      },
+    ],
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        allowList: {
+          db: true,
+          docs: true,
+          env: true,
+          err: true,
+          i: true,
+          param: true,
+          req: true,
+          res: true,
+        },
+      },
+    ],
+  },
 };
