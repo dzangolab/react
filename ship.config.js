ebb331774439a74f1fa979fc369840c97e433456
Sudeep Bashistha <32100152+BashisthaSudeep@users.noreply.github.com>
2022-12-13 17:44:45 +0545

    feat: add ship.js config and github workflow (#2)

---
+++ ship.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  appName: "@dzangolab/react",
+  buildCommand: ({ isYarn, version }) => {
+    return "pnpm build";
+  },
+  installCommand: ({ isYarn }) => {
+    return "pnpm -r install";
+  },
+  monorepo: {
+    mainVersionFile: "package.json",
+    packagesToBump: ["packages/*", "tools/*"],
+    packagesToPublish: ["packages/*"],
+  },
+  publishCommand: ({ isYarn, tag, defaultCommand, dir }) => {
+    return "pnpm publish --access public";
+  },
+};
