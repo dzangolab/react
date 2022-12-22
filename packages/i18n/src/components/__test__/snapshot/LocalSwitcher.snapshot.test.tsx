6ee7b42ba91630b246230acd41f0563559875f11
Sudeep Bashistha <32100152+BashisthaSudeep@users.noreply.github.com>
2022-12-22 15:01:48 +0545

    feat: add react-i18n package to monorepo (#24)

---
+++ packages/i18n/src/components/__test__/snapshot/LocalSwitcher.snapshot.test.tsx
@@ -0,0 +1,38 @@
+import renderer from "react-test-renderer";
+import { expect, test } from "vitest";
+
+import i18n from "../../../index";
+import LocaleSwitcher from "../../LocaleSwitcher";
+
+function toJson(component: renderer.ReactTestRenderer) {
+  const result = component.toJSON();
+
+  expect(result).toBeDefined();
+  expect(result).not.toBeInstanceOf(Array);
+
+  return result as renderer.ReactTestRendererJSON;
+}
+
+test("Component matches snapshot", () => {
+  const i18nConfig = {
+    appendNamespaceToCIMode: true,
+    debug: true,
+    defaultNS: "app",
+    fallbackLng: "en",
+    lng: "en",
+    resources: {
+      en: {},
+      fr: {},
+    },
+    supportedLngs: ["en", "fr"],
+    react: {
+      useSuspense: false,
+    },
+    locales: "en:English,fr:Français",
+  };
+
+  i18n(i18nConfig);
+  const component = renderer.create(<LocaleSwitcher />);
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});
