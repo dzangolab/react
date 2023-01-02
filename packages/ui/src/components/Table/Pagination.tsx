29fe1ea1d67767a711cd78dde8fa4276a3d29a50
Kiran Ojha <kiranojha1226@gmail.com>
2023-01-02 09:39:22 +0545

    feat(ui): add pagination component

---
+++ packages/ui/src/components/Table/Pagination.tsx
@@ -0,0 +1,26 @@
+import { useContext, useId } from "react";
+
+import { TableContext } from "./TableProvider";
+
+import type { PaginationProperties } from "./types";
+
+const Pagination = (properties: PaginationProperties) => {
+  const id = useId();
+  const { loading } = useContext(TableContext);
+  return (
+    <div className="pagination-container">
+      {properties.config.map(({ icon, isDisabled, onClick }, index) => (
+        <button
+          key={id + index}
+          className="pagination-button"
+          onClick={onClick}
+          disabled={isDisabled || loading}
+        >
+          {icon && icon}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
