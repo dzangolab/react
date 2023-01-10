d160c11f5e8306ad7f664a8ac0321f0a94afe764
Kiran Ojha <kiranojha1226@gmail.com>
2023-01-10 10:15:08 +0545

    refactor(user):  use path alias for auth

--- packages/user/src/index.ts
+++ packages/user/src/index.ts
@@ -4,7 +4,7 @@ import AuthGoogleCallback from "./components/AuthGoogleCallback";
 import DropdownUserMenu from "./components/DropdownUserMenu";
 import UserMenu from "./components/UserMenu";
 import UserProvider, { userContext } from "./context/UserProvider";
-import authReducer, { getUser } from "./redux/reducer/authslice";
+import authReducer, { getUser } from "@/redux/reducer/authslice";
 import superTokens from "./supertokens";
 import UserToastContainer from "./toastify";
 import { UserContextType } from "./types";
