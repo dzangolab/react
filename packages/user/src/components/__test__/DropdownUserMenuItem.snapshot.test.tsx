af5bbc4dcb86b1530f306280efd87d4fec3348de
Kiran Ojha <kiranojha1226@gmail.com>
2023-01-16 09:15:50 +0545

    refactor(user): refactor type import

--- packages/user/src/components/__test__/DropdownUserMenuItem.snapshot.test.tsx
+++ packages/user/src/components/__test__/DropdownUserMenuItem.snapshot.test.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { create } from "react-test-renderer";
 import { expect, test } from "vitest";
 
 import DropdownUserMenuItem from "../DropdownUserMenuItem";
 
-function toJson(component: renderer.ReactTestRenderer) {
+import type {
+  ReactTestRenderer,
+  ReactTestRendererJSON,
+} from "react-test-renderer";
+
+function toJson(component: ReactTestRenderer) {
   const result = component.toJSON();
 
   expect(result).toBeDefined();
   expect(result).not.toBeInstanceOf(Array);
 
-  return result as renderer.ReactTestRendererJSON;
+  return result as ReactTestRendererJSON;
 }
 
 test("Component matches snapshot", () => {
-  const component = renderer.create(
-    <DropdownUserMenuItem>test</DropdownUserMenuItem>
-  );
+  const component = create(<DropdownUserMenuItem>test</DropdownUserMenuItem>);
   const tree = toJson(component);
   expect(tree).toMatchSnapshot();
 });
