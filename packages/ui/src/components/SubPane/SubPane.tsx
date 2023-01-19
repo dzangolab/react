bb83956232fb01982f9cbd0538843d522981eed7
Kiran Ojha <kiranojha1226@gmail.com>
2023-01-19 16:47:21 +0545

    refactor(ui): extract subpane component

---
+++ packages/ui/src/components/SubPane/SubPane.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from "react";
+
+type Properties = {
+  children: ReactNode;
+  title: string;
+  icon: string;
+};
+
+const SubPane: React.FC<Properties> = ({ children }) => {
+  return (
+    <div role="region" className="sub-pane">
+      {children}
+    </div>
+  );
+};
+
+export default SubPane;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -5,6 +5,7 @@ import { LoadingIcon } from "./components/Loading";
 import Page from "./components/Page";
 import ResponsiveMenu from "./components/ResponsiveMenu";
 import { Tab, Tabs, TabTitle } from "./components/Tab";
+import { SubPane } from "./components/SubPane";
 
 export {
   Accordion,
@@ -15,6 +16,7 @@ export {
   LoadingIcon,
   Page,
   ResponsiveMenu,
+  SubPane,
   Tab,
   Tabs,
   TabTitle,
