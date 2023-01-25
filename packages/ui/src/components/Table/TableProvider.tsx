e92a5abe260d17acde5a34a47c95b9a3babbe8f6
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2023-01-25 13:32:41 +0545

    React/UI: reusable table (#67)
    * chore(ui): add necessary packages
    
    * feat(ui): add utility functions to format table state to json
    
    * feat(ui): add types
    
    * style(ui): add default css
    
    * feat(ui): add table provider
    
    * feat(ui): add table header component
    
    * feat(ui): add table body component
    
    * feat(ui): add column filter component
    
    * feat(ui): add pagination component
    
    * feat(ui): add base table with server side operations
    
    * feat(ui): export components
    
    * feat: export components from ui library
    
    * chore: update lockfile to match package.json
    
    * feat(ui): allow custom pagination icons
    
    * feat(ui): enable custom sort icons
    
    * feat(ui): enable custom filter icons
    
    * feat(ui): add option to configure multi sort
    
    * refactor(ui): show filter only for string
    
    * feat(ui): export columndef
    
    * feat(ui): export requestjson type
    
    * feat(ui): use template literals
    
    * fix(ui): refactor code
    
    * refactor(ui): hide warnings
    
    * refactor(ui): use generics to get data
    
    * fix(ui): fix round off issue with pagecount

---
+++ packages/ui/src/components/Table/TableProvider.tsx
@@ -0,0 +1,92 @@
+import React, { createContext, useCallback, useRef, useState } from "react";
+
+import BaseTable from "./BaseTable";
+
+import type {
+  TableContextProperties,
+  TableProviderProperties,
+  TRequestJSON,
+} from "./types";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const TableContext = createContext<TableContextProperties<any>>({
+  columns: [],
+  data: [],
+  loading: false,
+  totalItems: 0,
+  fetchCallback: () => {
+    return;
+  },
+});
+
+function TableProvider<T>({
+  fetcher,
+  children,
+  ...rest
+}: TableProviderProperties<T>) {
+  const [data, setData] = useState<T[]>([]);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const [loading, setLoading] = useState(false);
+  const [totalItems, setTotalItems] = useState<number>(0);
+  const fetchIdReference = useRef(0);
+
+  const fetchCallback = useCallback(
+    async (requestJSON: TRequestJSON) => {
+      const fetchId = ++fetchIdReference.current;
+      try {
+        if (fetchId === fetchIdReference.current) {
+          setLoading(true);
+          const data = await fetcher(requestJSON);
+          setTotalItems(data.totalItems);
+          setData(data.data);
+        }
+      } catch (error) {
+        let message = "Unknown Error";
+        if (error instanceof Error) message = error.message;
+        setErrorMessage(message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [fetcher]
+  );
+
+  return (
+    <TableContext.Provider
+      value={{ loading, data, totalItems, fetchCallback, ...rest }}
+    >
+      {children ? children : <BaseTable />}
+    </TableContext.Provider>
+  );
+}
+
+TableProvider.defaultProps = {
+  enableMultiSort: false,
+  filterDebounceTime: 300,
+  filterIcons: {
+    expanded: "🙉",
+    notExpanded: "🙈",
+  },
+  fixedHeader: true,
+  hideScrollBar: true,
+  paginated: true,
+  paginationIcons: {
+    start: "<<",
+    previous: "<",
+    next: ">",
+    end: ">>",
+  },
+  rowsPerPageOptions: [10, 20, 30],
+  showLoading: true,
+  showPageControl: true,
+  showTotalNumber: true,
+  sortable: true,
+  sortIcons: {
+    asc: "🔼",
+    desc: "🔽",
+    default: "↕️",
+  },
+};
+
+export default TableProvider;
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,14 +6,24 @@ import Page from "./components/Page";
 import ResponsiveMenu from "./components/ResponsiveMenu";
 import { SubPane } from "./components/SubPane";
 import { TabbedPanel } from "./components/TabbedPanel";
+import { BaseTable, Table } from "./components/Table";
+
+import type {
+  TableColumnDefinition,
+  TRequestJSON,
+} from "./components/Table/types";
 
 export {
   Accordion,
+  BaseTable,
   Collapse,
   LoadingButton,
   LoadingIcon,
   Page,
   ResponsiveMenu,
   SubPane,
+  Table,
   TabbedPanel,
 };
+
+export type { TableColumnDefinition, TRequestJSON };
