8bc5ac10156b6bfb72a736634952e31867d402fb
Kiran Ojha <kiranojha1226@gmail.com>
2023-02-10 13:29:59 +0545

    feat: run storybook from root

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "stories": [
+    "../packages/layout/src/**/*.stories.@(tsx)",
+    "../packages/ui/src/**/*.stories.@(tsx)",
+  ],
   "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   "framework": {
     name: "@storybook/react-vite",
