30471a5a8efe095cc625444a751e69f22d5dbc77
Kiran Ojha <kiranojha1226@gmail.com>
2023-02-14 13:24:33 +0545

    refactor(ui): move complicated components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,17 +1,14 @@
-import { Accordion } from "./components/Accordion";
-import { LoadingButton } from "./components/Buttons";
-import Collapse from "./components/Collapse";
-import { LoadingIcon, LoadingPage } from "./components/Loading";
-import Page from "./components/Page";
-import ResponsiveMenu from "./components/ResponsiveMenu";
+import { Accordion } from "./Accordion";
+import { LoadingButton } from "./Buttons";
+import Collapse from "./Collapse";
 import { SubPane } from "./components/SubPane";
-import { TabbedPanel } from "./components/TabbedPanel";
-import { BaseTable, Table } from "./components/Table";
+import { LoadingIcon, LoadingPage } from "./Loading";
+import Page from "./Page";
+import ResponsiveMenu from "./ResponsiveMenu";
+import { TabbedPanel } from "./TabbedPanel";
+import { BaseTable, Table } from "./Table";
 
-import type {
-  TableColumnDefinition,
-  TRequestJSON,
-} from "./components/Table/types";
+import type { TableColumnDefinition, TRequestJSON } from "./Table/types";
 
 export {
   Accordion,
