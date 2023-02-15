7e249e3e9959f8ca3f2ce7498892ae7c06d36c1f
shailesh Mishra <shaileshme57@gmail.com>
2023-02-15 19:24:30 +0545

    chore: remove unused imports

--- app/demo/src/Views/Home/Home.test.tsx
+++ app/demo/src/Views/Home/Home.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from "@testing-library/react";
-import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import { test } from "vitest";
 
 import Home from ".";
 
