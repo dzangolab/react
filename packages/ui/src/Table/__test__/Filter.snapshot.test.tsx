a8cb644099301209d9437a86f13d12a4ed07dba5
Kiran Ojha <kiranojha1226@gmail.com>
2023-02-21 18:16:25 +0545

    test(ui): add snapshot test for filter

---
+++ packages/ui/src/Table/__test__/Filter.snapshot.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import renderer, { create } from "react-test-renderer";
+import { expect, test, vi } from "vitest";
+
+import Filter from "../Filter";
+
+const mockFunction = vi.fn();
+
+function toJson(component: renderer.ReactTestRenderer) {
+  const result = component.toJSON();
+
+  expect(result).toBeDefined();
+  expect(result).not.toBeInstanceOf(Array);
+
+  return result as renderer.ReactTestRendererJSON;
+}
+
+test("Component matches snapshot", () => {
+  const component = create(
+    <Filter
+      columnType="number"
+      columnFilterValue=""
+      handleChange={mockFunction}
+    />
+  );
+  const tree = toJson(component);
+  expect(tree).toMatchSnapshot();
+});
