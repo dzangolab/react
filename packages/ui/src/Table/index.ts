e0f10f2e3ff94834dfbcbe91d7de44d68c2278e0
Kiran Ojha <kiranojha1226@gmail.com>
2023-02-28 10:23:48 +0545

    refactor(ui): rename table provider with appropriate name

--- packages/ui/src/Table/index.ts
+++ packages/ui/src/Table/index.ts
@@ -4,14 +4,14 @@ import BaseTable from "./common/BaseTable";
 import { TableContext } from "./common/TableContext";
 import TableDetail from "./common/TableDetail";
 import TableFooter from "./common/TableFooter";
-import TableProvider from "./TableProvider";
+import ServerTableProvider from "./ServerTableProvider";
 
 export {
   BaseTable,
   TableContext,
   TableDetail,
   TableFooter,
-  TableProvider as Table,
+  ServerTableProvider as Table,
 };
 
 export type { TableColumnDefinition, TRequestJSON } from "./common/types";
