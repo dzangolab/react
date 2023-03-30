279fca563a0f0b1cb45ebe28053d561ea9c525ff
shailesh Mishra <shaileshme57@gmail.com>
2023-03-30 09:17:53 +0545

    feat: add submit button and responsive menu

---
+++ apps/demo/src/Views/Ui/SubmitButton.tsx
@@ -0,0 +1,10 @@
+import { SubmitButton as Button } from "@dzangolab/react-ui";
+
+export const SubmitButton = () => {
+  return (
+    <>
+      <Button label="Submit" />
+      <Button label="Submit loading" loading />
+    </>
+  );
+};
--- apps/demo/src/constants.ts
+++ apps/demo/src/constants.ts
@@ -1,9 +1,24 @@
 enum UI_COMPONENTS_MAPPING {
   LOADING_ICON = 1,
+  SUBMIT_BUTTON = 2,
+  RESPONSIVE_MENU = 3,
 }
 
 const UI_COMPONENTS = [
   { key: UI_COMPONENTS_MAPPING.LOADING_ICON, value: "Loading Icon" },
+  { key: UI_COMPONENTS_MAPPING.SUBMIT_BUTTON, value: "Submit Button" },
+  { key: UI_COMPONENTS_MAPPING.RESPONSIVE_MENU, value: "Responsive menu" },
 ];
 
-export { UI_COMPONENTS, UI_COMPONENTS_MAPPING };
+const MENU_ROUTES = [
+  {
+    name: "Home",
+    route: "/ui",
+  },
+  {
+    name: "About",
+    route: "/ui",
+  },
+];
+
+export { UI_COMPONENTS, UI_COMPONENTS_MAPPING, MENU_ROUTES };
