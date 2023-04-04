3133edc4bfd7c0fc6d48f8c45a4f6e150dae4e0b
shailesh Mishra <shaileshme57@gmail.com>
2023-04-04 16:50:31 +0545

    feat: add reusable dropdown menu component

---
+++ apps/demo/src/Views/index.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef, useState } from "react";
+
+import "./index.css";
+
+interface IProperties {
+  label: string;
+  dropdownMenu: React.ReactNode;
+}
+
+const DropdownNavMenu: React.FC<IProperties> = ({ label, dropdownMenu }) => {
+  const [expanded, setExpanded] = useState(false);
+  const navBarReference = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        navBarReference.current &&
+        !navBarReference.current.contains(event.target as Node)
+      ) {
+        setExpanded(false);
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <nav
+      ref={navBarReference}
+      className={`dropdown-nav-menu ${expanded ? "expanded" : ""}`}
+    >
+      <div className="label" onClick={() => setExpanded(!expanded)}>
+        {label}
+        <span className={"truncated"}>
+          {expanded ? <>&#9650;</> : <>&#9662;</>}
+        </span>
+      </div>
+
+      {expanded && <ul className="dropdown">{dropdownMenu}</ul>}
+    </nav>
+  );
+};
+export default DropdownNavMenu;
