7a477056b91d969061f56609e5c3b6ef186ca0da
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2023-04-07 14:08:57 +0545

    feat: google button ui (#292)
    * feat: google button ui
    
    * refactor(user): to support the new social login ui
    
    * fix(user): lint warnings
    
    * refactor(user): move divider
    
    * feat: button hover effect
    
    * refactor: class name
    
    * feat: add sso base button
    
    * chore: remove var
    
    * fix: button center style

---
+++ packages/ui/src/SSOButtons/GoogleButton/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import GoogleIcon from "../../assets/images/google-icon.svg";
+import BaseButton, { BaseButtonProperties } from "../BaseButton";
+
+import "./index.css";
+
+type IProperties = Omit<BaseButtonProperties, "imageSource" | "className">;
+
+const GoogleButton: React.FC<IProperties> = (properties) => {
+  return (
+    <BaseButton className="google" imageSource={GoogleIcon} {...properties} />
+  );
+};
+
+export default GoogleButton;
