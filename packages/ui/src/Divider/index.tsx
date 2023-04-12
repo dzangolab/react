b133fdb29e40e5e517507e99991fcb66cf3bb9fe
shailesh Mishra <shaileshme57@gmail.com>
2023-04-12 15:32:16 +0545

    refactor: use class to style divider

--- packages/ui/src/Divider/index.tsx
+++ packages/ui/src/Divider/index.tsx
@@ -1,35 +1,19 @@
-import React, { CSSProperties } from "react";
+import React from "react";
 
-interface IProperties
-  extends Pick<
-    CSSProperties,
-    "color" | "width" | "height" | "borderStyle" | "margin"
-  > {
+import "./index.css";
+
+interface IProperties {
   orientation?: "horizontal" | "vertical";
 }
 
-const Divider: React.FC<IProperties> = ({
-  color = "black",
-  width,
-  height,
-  borderStyle = "solid",
-  margin,
-  orientation = "horizontal",
-}) => {
-  const horizontalStyle = {
-    width: width,
-    border: `${height} ${borderStyle} ${color}`,
-    margin: margin ? margin : "1rem 0",
-  };
-  const verticalStyle = {
-    border: `${width} ${borderStyle} ${color}`,
-    height: height ? height : "100%",
-    margin: margin ? margin : "0 1rem",
-    width: 0,
-  };
-  const style = orientation === "horizontal" ? horizontalStyle : verticalStyle;
-
-  return <hr style={style} />;
+const Divider: React.FC<IProperties> = ({ orientation = "horizontal" }) => {
+  return (
+    <div
+      role="separator"
+      className="divider"
+      data-aria-orientation={orientation}
+    />
+  );
 };
 
 export default Divider;
