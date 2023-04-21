660cb6bc8fb5fa51b70b05e25997bd0634e1cd1c
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2023-04-21 16:07:36 +0545

    refactor: replace divider-with-text with divider (#334)

--- apps/demo/src/Views/User/index.tsx
+++ apps/demo/src/Views/User/index.tsx
@@ -1,10 +1,47 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { Page } from "@dzangolab/react-ui";
+import { Login } from "@dzangolab/react-user";
+import { useState } from "react";
+
+import Card from "../../components/Card";
+import PageWrapper from "../../components/PageWrapper";
+
+const ROUTES = [
+  {
+    key: 1,
+    title: "Login Page",
+    component: <Login orientation="horizontal" />,
+  },
+];
 
 const User = () => {
   const { t } = useTranslation();
+  const [selectedComponentIndex, setSelectedComponentIndex] = useState<
+    null | number
+  >(null);
 
-  return <Page title={t("header.menu.user")}></Page>;
+  const renderComponent = () => {
+    if (selectedComponentIndex === null) {
+      return ROUTES.map((component, index) => (
+        <Card
+          key={component.key}
+          title={component.title}
+          onClick={() => setSelectedComponentIndex(index)}
+        />
+      ));
+    } else {
+      return (
+        <PageWrapper
+          onBack={() => setSelectedComponentIndex(null)}
+          title={ROUTES[selectedComponentIndex].title}
+        >
+          {ROUTES[selectedComponentIndex].component}
+        </PageWrapper>
+      );
+    }
+  };
+
+  return <Page title={t("header.menu.user")}>{renderComponent()}</Page>;
 };
 
 export default User;
--- apps/demo/src/config.ts
+++ apps/demo/src/config.ts
@@ -65,6 +65,7 @@ const config: AppConfig = {
       appName: "",
       appURL: "",
     },
+    supportedLoginProviders: ["google"],
   },
   websiteDomain: (window.location.protocol +
     "//" +
