c0598c682c8387e6954ad1a53fe7eea31f69df8c
shailesh Mishra <shaileshme57@gmail.com>
2023-05-30 17:52:45 +0545

    feat: agree terms and condition component

--- apps/demo/src/Views/Ui/index.tsx
+++ apps/demo/src/Views/Ui/index.tsx
@@ -4,6 +4,7 @@ import { LoadingIcon, Page, ResponsiveMenu } from "@dzangolab/react-ui";
 import { SubmitButton } from "./SubmitButton";
 import ComponentList from "../../components/ComponentList";
 import { MENU_ROUTES } from "../../constants";
+import TermsAndCondition from "./TermsAndCondition";
 
 const componentList = [
   {
@@ -40,6 +41,11 @@ const componentList = [
         </Page>
       </>
     ),
+  },
+  {
+    key: 4,
+    title: "Terms and conditions",
+    component: <TermsAndCondition name="termsAndCondition" />,
   },
 ];
 
