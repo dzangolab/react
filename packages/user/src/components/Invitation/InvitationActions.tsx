c39c0ce0dd22a45eedbd65be723366a8d9629a80
Manish <manish.aryal@dzangolab.com>
2023-07-20 16:46:39 +0545

    refactor: update translation files for resend and revoke label

--- packages/user/src/components/Invitation/InvitationActions.tsx
+++ packages/user/src/components/Invitation/InvitationActions.tsx
@@ -1,5 +1,6 @@
 import { MenuItem } from "primereact/menuitem";
 import { ActionsMenu } from "@dzangolab/react-ui";
+import { useTranslation } from "@dzangolab/react-i18n";
 
 type InvitationActionsProperites = {
   handleInvitationResend?: (data: any) => void;
@@ -12,10 +13,12 @@ export const InvitationActions = ({
   handleInvitationRevoke,
   data,
 }: InvitationActionsProperites) => {
+  const { t } = useTranslation("user");
+
   const actionItems: MenuItem[] = [];
   if (handleInvitationResend) {
     actionItems.push({
-      label: "Resend",
+      label: t("invitation.actions.resend"),
       icon: "pi pi-replay",
       command: (event) => {
         handleInvitationResend(data);
@@ -25,7 +28,7 @@ export const InvitationActions = ({
 
   if (handleInvitationRevoke) {
     actionItems.push({
-      label: "Revoke",
+      label: t("invitation.actions.revoke"),
       icon: "pi pi-times",
       className: "danger",
       command: (event) => {
