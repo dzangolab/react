026cec86e8d1b0dc715874ee92faf76716cc1b90
Manish <manish.aryal@dzangolab.com>
2023-08-02 17:03:21 +0545

    feat: add source to image and render file name conditionally

--- packages/ui/src/UploadFileWidget/index.tsx
+++ packages/ui/src/UploadFileWidget/index.tsx
@@ -6,22 +6,6 @@ export const UploadFile = () => {
   const [customFileName, setCustomFileName] = useState("");
   const [totalSize, setTotalSize] = useState(0);
 
-  const handleFileChange = (event: any) => {
-    setCustomFileName(event.target.value);
-  };
-
-  const handleUpload = ({ files }: any) => {
-    // Assuming you want to upload a single file
-    const file = files && files.length > 0 ? files[0] : null;
-
-    if (file) {
-      const formData = new FormData();
-      formData.append("file", file, customFileName || file.name);
-      console.log(formData);
-      // Perform the upload using your API or server logic
-      // Example: axios.post("/api/upload", formData);
-    }
-  };
   const onTemplateRemove = (file: File, callback: any) => {
     setTotalSize(totalSize - file.size);
     callback();
@@ -38,20 +22,22 @@ export const UploadFile = () => {
           <img
             alt="/"
             role="presentation"
-            src={file.webkitRelativePath}
+            src={URL.createObjectURL(file)}
             width={100}
           />
           <span className="flex flex-column text-left ml-3">
-            {file.name}
+            {customFileName ? customFileName : file.name}
             <small>{new Date().toLocaleDateString()}</small>
           </span>
         </div>
+
         <input
           type="text"
           value={customFileName}
           onChange={(event) => setCustomFileName(event.target.value)}
           placeholder="Enter custom file name"
         />
+
         <Button
           type="button"
           icon="pi pi-times"
@@ -70,7 +56,6 @@ export const UploadFile = () => {
         url={"/api/upload"}
         multiple
         accept="image/*"
-        onUpload={handleUpload}
         emptyTemplate={
           <p className="m-0">Drag and drop files to here to upload.</p>
         }
