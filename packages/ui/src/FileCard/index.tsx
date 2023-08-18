920ca4f8e947d89d902ae079b46352f0f79b96f8
Manish <manish.aryal@dzangolab.com>
2023-08-18 11:24:00 +0545

    refactor: make head body and footer part for the card

--- packages/ui/src/FileCard/index.tsx
+++ packages/ui/src/FileCard/index.tsx
@@ -2,16 +2,34 @@ import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 
 export const FileCard = () => {
+  const header = () => {
+    return <></>;
+  };
+
+  const body = () => {
+    return (
+      <p className="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
+        consequuntur error repudiandae numquam deserunt quisquam repellat libero
+        asperiores earum nam nobis, culpa ratione quam perferendis esse,
+        cupiditate neque quas!
+      </p>
+    );
+  };
+
+  const footer = () => {
+    return <></>;
+  };
   return (
     <div className="">
-      <Card title="Title" subTitle="Subtitle" className="md:w-25rem">
-        <p className="m-0">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
-          sed consequuntur error repudiandae numquam deserunt quisquam repellat
-          libero asperiores earum nam nobis, culpa ratione quam perferendis
-          esse, cupiditate neque quas!
-        </p>
-      </Card>
+      <Card
+        title="Title"
+        subTitle="Subtitle"
+        className="md:w-25rem"
+        header={header}
+        children={body()}
+        footer={footer}
+      />
     </div>
   );
 };
