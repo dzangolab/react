28004fc2d07c83d051311da99cb3b9a9335b1a4a
Manish <manish.aryal@dzangolab.com>
2023-08-21 14:00:09 +0545

    feat: add class file and style for file card component

--- packages/ui/src/FileCard/index.tsx
+++ packages/ui/src/FileCard/index.tsx
@@ -21,17 +21,17 @@ export const FileCard = ({
         <span className="file-description">This is my file</span>
         <span className="file-size">14mb</span>
       </div>
-      <div className="file-upload-detail">
+      <div className="file-upload-details">
         <div className="uploaded-by">
           <span>Uploaded by</span>
           <span>Manish</span>
         </div>
-        <div className="uploaded-at-detail">
+        <div className="uploaded-at">
           <span>Uploaded At</span>
           <span>5678</span>
         </div>
       </div>
-      <div className="file-download-detail">
+      <div className="file-download-details">
         <div className="download-count">
           <span>Download count</span>
           <span>24</span>
@@ -41,7 +41,7 @@ export const FileCard = ({
           <span>67890</span>
         </div>
       </div>
-      <div className="actions">
+      <div className="file-actions">
         <Button icon="pi pi-download" label="Download" />
         <Button icon="pi pi-eye" label="View" />
       </div>
