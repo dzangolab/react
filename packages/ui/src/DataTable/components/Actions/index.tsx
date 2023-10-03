1eccaaced0352d62e673cdc0918530a8031cadee
lamdiki-sherpa <lamdiki.sherpa@dzangolab.com>
2023-10-03 11:17:38 +0545

    feat(confirmation-modal): change type of prop  data

--- packages/ui/src/DataTable/components/Actions/index.tsx
+++ packages/ui/src/DataTable/components/Actions/index.tsx
@@ -3,13 +3,13 @@ import { ButtonProps } from "primereact/button";
 import { MenuItem } from "primereact/menuitem";
 import React, { useState } from "react";
 
-import { ConfirmationModal } from "../../../ConfirmationModal";
-import { Menu } from "../../../Menu";
+import { ConfirmationModal } from "@/ConfirmationModal";
+import { Menu } from "@/Menu";
 
 export interface ActionsMenuProperties {
   actions?: MenuItem[];
   buttonOptions?: Omit<ButtonProps, "onClick">;
-  data?: any;
+  data?: object;
   deleteIcon?: string;
   deleteLabel?: string;
   editIcon?: string;
