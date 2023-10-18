a156ff35cd4a0c189c7f3f229564d33a70b5840f
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-10-18 17:32:15 +0545

    feat(create-thing): add props to switch-input-component

--- packages/form/src/components/SwitchInput.tsx
+++ packages/form/src/components/SwitchInput.tsx
@@ -1,13 +1,39 @@
 import { InputSwitch } from "primereact/inputswitch";
 import React, { useState } from "react";
+import { UseFormGetFieldState, UseFormRegister } from "react-hook-form";
 
-export const SwitchInput = () => {
+import { ErrorMessage } from "./ErrorMessage";
+interface ISwitch {
+  label?: string;
+  name: string;
+  getFieldState?: UseFormGetFieldState<any>;
+  register?: UseFormRegister<any>;
+}
+export const SwitchInput: React.FC<ISwitch> = ({
+  name,
+  label,
+  getFieldState,
+  register,
+}) => {
   const [checked, setChecked] = useState(true);
+  if (!register || !getFieldState) return null;
+
+  const { error, isDirty, isTouched, invalid } = getFieldState(name);
+
+  let switchClassName = "";
+  if (isDirty && !invalid) switchClassName = "valid";
+  if (isTouched && invalid) switchClassName = "invalid";
 
   return (
-    <InputSwitch
-      checked={checked}
-      onChange={() => setChecked((previous: boolean) => !previous)}
-    />
+    <>
+      {label && <label htmlFor={name}>{label}</label>}
+      <InputSwitch
+        {...register(name)}
+        checked={checked}
+        onChange={() => setChecked((previous: boolean) => !previous)}
+        className={switchClassName}
+      />
+      {error?.message && <ErrorMessage message={error.message} />}
+    </>
   );
 };
