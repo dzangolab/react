45fed2a00bb0186a4eed85f93624eee819a3f4bd
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-10-18 17:01:08 +0545

    feat(switch-input): update switch input component

--- packages/form/src/components/SwitchInput.tsx
+++ packages/form/src/components/SwitchInput.tsx
@@ -1,37 +1,13 @@
 import { InputSwitch } from "primereact/inputswitch";
-import { classNames } from "primereact/utils";
-import React from "react";
-import { Controller, useForm } from "react-hook-form";
+import React, { useState } from "react";
 
 export const SwitchInput = () => {
-  const defaultValues = {
-    checked: false,
-  };
-
-  const {
-    control,
-    formState: { errors },
-  } = useForm({ defaultValues });
+  const [checked, setChecked] = useState(true);
 
   return (
-    <Controller
-      name="checked"
-      control={control}
-      render={({ field, fieldState }) => (
-        <>
-          <label
-            htmlFor={field.name}
-            className={classNames({ "p-error": errors.checked })}
-          ></label>
-          <InputSwitch
-            inputId={field.name}
-            checked={field.value}
-            inputRef={field.ref}
-            className={classNames({ "p-invalid": fieldState.error })}
-            onChange={(event) => field.onChange(event.value)}
-          />
-        </>
-      )}
+    <InputSwitch
+      checked={checked}
+      onChange={() => setChecked((previous: boolean) => !previous)}
     />
   );
 };
