04ea1b43454af6513d6e1b33a9e002af09d15a6e
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-10-30 14:35:12 +0545

    feat(switch-component): modify switch component

--- packages/form/src/components/Switch.tsx
+++ packages/form/src/components/Switch.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { UseFormGetFieldState, UseFormRegister } from "react-hook-form";
 
-export const Switch = () => {
+import { ErrorMessage } from "./ErrorMessage";
+
+interface ISwitch {
+  label?: string;
+  name: string;
+  getFieldState?: UseFormGetFieldState<any>;
+  register?: UseFormRegister<any>;
+}
+
+export const Switch: React.FC<ISwitch> = ({
+  register,
+  getFieldState,
+  label = "",
+  name,
+}) => {
+  if (!register || !getFieldState) return null;
+
+  const { error, isDirty, isTouched, invalid } = getFieldState(name);
+
+  let switchClassName = "";
+  if (isDirty && !invalid) switchClassName = "valid";
+  if (isTouched && invalid) switchClassName = "invalid";
+
   return (
-    <label className="switch">
-      <input type="checkbox" />
-      <span className="slider round"></span>
-    </label>
+    <div className={`field switch-toggle ${name}`}>
+      {label && <label htmlFor={name}>{label}</label>}
+      <input
+        {...register(name)}
+        className={switchClassName}
+        type="checkbox"
+      ></input>
+      {error?.message && <ErrorMessage message={error.message} />}
+    </div>
   );
 };
