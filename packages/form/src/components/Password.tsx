41ad9e596989162396288c79eb1ab3ff28a18284
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-10-31 13:50:52 +0545

    feat(show-password-icon): change the css selector

--- packages/form/src/components/Password.tsx
+++ packages/form/src/components/Password.tsx
@@ -17,19 +17,23 @@ export const Password: React.FC<CustomInputProperties> = ({
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className={`field password-input ${name}`}>
+    <div className={`field ${name}`}>
       {label && <label htmlFor={`input-field-${name}`}>{label}</label>}
-      <input
-        {...register(name)}
-        id={`input-field-${name}`}
-        type={showPassword ? "text" : "password"}
-        placeholder={placeholder}
-        aria-invalid={submitcount > 0 ? invalid : undefined}
-        className="input-field-password "
-      ></input>
-      <span className="eye-icon" onClick={() => setShowPassword(!showPassword)}>
-        <i className={showPassword ? "pi pi-eye" : "pi pi-eye-slash"}></i>
-      </span>
+      <div className="input-field-password">
+        <input
+          {...register(name)}
+          id={`input-field-${name}`}
+          type={showPassword ? "text" : "password"}
+          placeholder={placeholder}
+          aria-invalid={submitcount > 0 ? invalid : undefined}
+        ></input>
+        <span
+          className="eye-icon"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          <i className={showPassword ? "pi pi-eye" : "pi pi-eye-slash"}></i>
+        </span>
+      </div>
       {error?.message && <ErrorMessage message={error.message} />}
     </div>
   );
