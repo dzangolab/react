90321d609f7e2633c01cf9aa09120195815c8a4f
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-01 10:21:20 +0545

    feat(files-table-css): add csss  for files table

--- packages/ui/src/FilesTable/index.tsx
+++ packages/ui/src/FilesTable/index.tsx
@@ -56,7 +56,7 @@ export type FilesTableProperties = {
 
 export const FilesTable = ({
   archiveConfirmationProps,
-  className,
+  className = "table-files",
   columns = [],
   deleteConfirmationProps,
   id,
