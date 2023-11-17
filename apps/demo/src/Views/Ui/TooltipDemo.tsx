ff85786f3b192e54779f58cbf374bf4855c4859c
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-17 17:53:14 +0545

    feat(tooltip-demo): add tooltip component with position bottom and delay for time duration

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -3,42 +3,18 @@ import { useRef } from "react";
 
 export const TooltipDemo = () => {
   const submitButtonReference = useRef<HTMLButtonElement>(null);
-  const cancelButtonReference = useRef<HTMLButtonElement>(null);
-  const buttonReference = useRef<HTMLButtonElement>(null);
-  const reference = useRef<HTMLButtonElement>(null);
+
   return (
     <>
       <Tooltip
         elementRef={submitButtonReference}
-        position="top"
+        position="bottom"
         offset={10}
         delay={200}
       >
         submit button
       </Tooltip>
       <button ref={submitButtonReference}>Submit</button>
-      <Tooltip
-        elementRef={cancelButtonReference}
-        position="bottom"
-        offset={5}
-        delay={100}
-      >
-        <span>cancel button</span>
-      </Tooltip>
-      <button ref={cancelButtonReference}>Cancel</button>
-      <Tooltip
-        elementRef={buttonReference}
-        position="left"
-        offset={5}
-        delay={200}
-      >
-        <span>Tooltip is displayed on left</span>
-      </Tooltip>
-      <button ref={buttonReference}>button</button>
-      <Tooltip elementRef={reference} position="right" offset={5}>
-        <span>Tooltip is displayed on right</span>
-      </Tooltip>
-      <button ref={reference}>right</button>
     </>
   );
 };
