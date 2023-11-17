e99073000ee3a5d8501e0b14fe339b137d5838be
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-17 17:10:44 +0545

    feat(tooltip-demo): add tooltip  demo for position bottom

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -3,6 +3,7 @@ import { useRef } from "react";
 
 export const TooltipDemo = () => {
   const submitButtonReference = useRef<HTMLButtonElement>(null);
+  const cancelButtonReference = useRef<HTMLButtonElement>(null);
   return (
     <>
       <Tooltip
@@ -14,6 +15,15 @@ export const TooltipDemo = () => {
         submit button
       </Tooltip>
       <button ref={submitButtonReference}>Submit</button>
+      <Tooltip
+        elementRef={cancelButtonReference}
+        position="bottom"
+        offset={5}
+        delay={100}
+      >
+        <span>cancel button</span>
+      </Tooltip>
+      <button ref={cancelButtonReference}>Cancel</button>
     </>
   );
 };
