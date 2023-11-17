6e09c199645c9d4c95a84d40bc7d143cf7373212
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-17 16:49:09 +0545

    feat(tooltip-demo): add delay as a props in tooltip component

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -5,7 +5,12 @@ export const TooltipDemo = () => {
   const submitButtonReference = useRef<HTMLButtonElement>(null);
   return (
     <>
-      <Tooltip elementRef={submitButtonReference} position="top" offset={10}>
+      <Tooltip
+        elementRef={submitButtonReference}
+        position="top"
+        offset={10}
+        delay={200}
+      >
         submit button
       </Tooltip>
       <button ref={submitButtonReference}>Submit</button>
