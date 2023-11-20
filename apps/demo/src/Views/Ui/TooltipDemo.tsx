6fd77d6a36929408b28dd491dba9b33e54873882
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-20 15:05:20 +0545

    feat(tooltip-demo): change the css selector

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -20,30 +20,27 @@ export const TooltipDemo = () => {
       >
         {t("tooltip.content")}
       </Tooltip>
-      <input ref={rightReference} placeholder={t("tooltip.positions.right")} />
+      <div ref={rightReference}>{t("tooltip.positions.right")}</div>
       <Tooltip elementRef={leftReference} position="left" delay={200}>
         {t("tooltip.content")}
       </Tooltip>
-      <input ref={leftReference} placeholder={t("tooltip.positions.left")} />
+      <div ref={leftReference}>{t("tooltip.positions.left")}</div>
       <Tooltip elementRef={topReference} position="top" offset={10} delay={500}>
         {t("tooltip.content")}
       </Tooltip>
-      <input ref={topReference} placeholder={t("tooltip.positions.top")} />
+      <div ref={topReference}>{t("tooltip.positions.top")}</div>
       <Tooltip elementRef={bottomReference} position="bottom">
         <h3>{t("tooltip.positions.bottom")}</h3>
         <h4>{t("tooltip.content")}</h4>
       </Tooltip>
-      <input
-        ref={bottomReference}
-        placeholder={t("tooltip.positions.bottom")}
-      />
-      <div className="button-save">
-        <Tooltip elementRef={buttonReference} position="right" offset={10}>
-          <i className="pi pi-check"></i>
-          <span>{t("tooltip.button.content")}</span>
-        </Tooltip>
-        <button ref={buttonReference}>{t("tooltip.button.save")}</button>
-      </div>
+      <div ref={bottomReference}>{t("tooltip.positions.bottom")}</div>
+      <Tooltip elementRef={buttonReference} position="right" offset={10}>
+        <i className="pi pi-check"></i>
+        <span>{t("tooltip.button.content")}</span>
+      </Tooltip>
+      <button ref={buttonReference} className="button-save">
+        {t("tooltip.button.save")}
+      </button>
     </div>
   );
 };
