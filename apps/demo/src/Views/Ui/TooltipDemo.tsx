74b1368a2d86eec9eb6a3901ecb13a235226f324
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-20 11:18:26 +0545

    feat(tooltip-demo): add tooltip component for different positions

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -4,19 +4,29 @@ import { useRef } from "react";
 
 export const TooltipDemo = () => {
   const { t } = useTranslation("ui");
-  const submitButtonReference = useRef<HTMLButtonElement>(null);
+  const rightReference = useRef(null);
+  const leftReference = useRef(null);
+  const topReference = useRef(null);
+  const bottomReference = useRef(null);
 
   return (
-    <>
-      <Tooltip
-        elementRef={submitButtonReference}
-        position="bottom"
-        offset={10}
-        delay={200}
-      >
-        {t("tooltip.submitButtonMessage")}
+    <div className="tooltip-component">
+      <Tooltip elementRef={rightReference} position="right" delay={100}>
+        {t("tooltip.positions.right.message")}
       </Tooltip>
-      <button ref={submitButtonReference}>Submit</button>
-    </>
+      <input ref={rightReference} placeholder="right" />
+      <Tooltip elementRef={leftReference} position="left" delay={200}>
+        {t("tooltip.positions.left.message")}
+      </Tooltip>
+      <input ref={leftReference} placeholder="left" />
+      <Tooltip elementRef={topReference} position="top" delay={100}>
+        {t("tooltip.positions.top.message")}
+      </Tooltip>
+      <input ref={topReference} placeholder="top" />
+      <Tooltip elementRef={bottomReference} position="bottom" delay={200}>
+        {t("tooltip.positions.bottom.message")}
+      </Tooltip>
+      <input ref={bottomReference} placeholder="bottom" />
+    </div>
   );
 };
