ace3c4df10cc562e112179a4330be329e848db1d
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-20 13:11:12 +0545

    feat(tooltip-demo): add style for tooltip in button

--- apps/demo/src/Views/Ui/TooltipDemo.tsx
+++ apps/demo/src/Views/Ui/TooltipDemo.tsx
@@ -8,10 +8,16 @@ export const TooltipDemo = () => {
   const leftReference = useRef(null);
   const topReference = useRef(null);
   const bottomReference = useRef(null);
+  const htmlReference = useRef(null);
 
   return (
     <div className="tooltip-component">
-      <Tooltip elementRef={rightReference} position="right" delay={100}>
+      <Tooltip
+        elementRef={rightReference}
+        position="right"
+        offset={10}
+        delay={100}
+      >
         {t("tooltip.positions.right.message")}
       </Tooltip>
       <input
@@ -25,20 +31,28 @@ export const TooltipDemo = () => {
         ref={leftReference}
         placeholder={t("tooltip.positions.left.placeholder")}
       />
-      <Tooltip elementRef={topReference} position="top" delay={100}>
+      <Tooltip elementRef={topReference} position="top" offset={10} delay={500}>
         {t("tooltip.positions.top.message")}
       </Tooltip>
       <input
         ref={topReference}
         placeholder={t("tooltip.positions.top.placeholder")}
       />
-      <Tooltip elementRef={bottomReference} position="bottom" delay={200}>
-        {t("tooltip.positions.bottom.message")}
+      <Tooltip elementRef={bottomReference} position="bottom">
+        <h3>{t("tooltip.positions.bottom.placeholder")}</h3>
+        <h4>{t("tooltip.positions.bottom.message")}</h4>
       </Tooltip>
       <input
         ref={bottomReference}
         placeholder={t("tooltip.positions.bottom.placeholder")}
       />
+      <div className="button-tooltip">
+        <Tooltip elementRef={htmlReference} position="right" offset={10}>
+          <i className="pi pi-check"></i>
+          <span>{t("tooltip.button.message")}</span>
+        </Tooltip>
+        <button ref={htmlReference}>{t("tooltip.button.save")}</button>
+      </div>
     </div>
   );
 };
