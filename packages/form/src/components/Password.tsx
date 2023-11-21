30168e1b975b4e359a7e6d7fcb4bfee68fd80425
Manish <manish.aryal@dzangolab.com>
2023-11-21 13:01:19 +0545

    fix(passwordInput): add padding for the input field (#643)
    * fix(passwordInput): add padding for the input field
    
    * fix(passworInput): add additional styling and remove div container
    
    * fix(passwordInput): change the order of styling
    
    * fix(passwordInput): add aria styling and attributes to the div container
    
    * fix(passwordInput): add focus on the div element
    
    * fix(passwordInput): add state for handling focus in div container
    
    * fix(passwordInput): add global configuration for outline
    
    * fix(passwordInput): update snapshot to fix the test issue

--- packages/form/src/components/Password.tsx
+++ packages/form/src/components/Password.tsx
@@ -15,17 +15,31 @@ export const Password: React.FC<CustomInputProperties> = ({
 
   const { error, invalid } = getFieldState(name);
   const [showPassword, setShowPassword] = useState(false);
+  const [className, setClassName] = useState<string>("input-field-password");
+
+  const handleFocus = () => {
+    setClassName("input-field-password focused");
+  };
+
+  const handleBlur = () => {
+    setClassName("input-field-password");
+  };
 
   return (
     <div className={`field ${name}`}>
       {label && <label htmlFor={`input-field-${name}`}>{label}</label>}
-      <div className="input-field-password">
+      <div
+        className={className}
+        aria-invalid={submitcount > 0 ? invalid : undefined}
+      >
         <input
           {...register(name)}
           id={`input-field-${name}`}
           type={showPassword ? "text" : "password"}
           placeholder={placeholder}
           aria-invalid={submitcount > 0 ? invalid : undefined}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
         ></input>
         <span
           className="eye-icon"
