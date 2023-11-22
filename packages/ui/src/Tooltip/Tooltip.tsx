64938899a3baa383524a88ce6445607281a7972a
Nabin Dhital <34764801+Nabin99@users.noreply.github.com>
2023-11-22 15:28:40 +0545

    feat(ui/tooltip): add global configuration option (#647)
    * feat(ui/tooltip): add global configuration option
    
    * refactor(app/demo): remove unwanted comment

---
+++ packages/ui/src/Tooltip/Tooltip.tsx
@@ -0,0 +1,95 @@
+import React, { RefObject, useRef, FC, useEffect } from "react";
+import { createPortal } from "react-dom";
+
+import { tooltipConfig } from "./ConfigureTooltip";
+import { useTooltip } from "./UseTooltip";
+
+type TooltipProperties = {
+  children: React.ReactNode;
+  className?: string;
+  delay?: number;
+  elementRef: RefObject<HTMLElement>;
+  mouseTrack?: boolean;
+  offset?: number;
+  position?: "top" | "bottom" | "right" | "left";
+  style?: object;
+};
+
+export const Tooltip: FC<TooltipProperties> = ({
+  children,
+  className,
+  delay = tooltipConfig.delay,
+  elementRef,
+  mouseTrack = tooltipConfig.mouseTrack,
+  offset = tooltipConfig.offset,
+  position = tooltipConfig.position,
+  style,
+}) => {
+  const tooltipReference = useRef<HTMLDivElement>(null);
+
+  const {
+    tooltipPosition,
+    showTooltip,
+    onMouseEnter,
+    onMouseLeave,
+    onMouseMove,
+  } = useTooltip({
+    delay,
+    mouseTrack,
+    offset,
+    position,
+    ref: elementRef,
+    tooltipReference,
+  });
+
+  const renderClassName = (mouseTrack?: boolean, position?: string) => {
+    if (mouseTrack) {
+      return "tooltip-container";
+    } else {
+      return `tooltip-container ${position}`;
+    }
+  };
+
+  useEffect(() => {
+    const element = elementRef?.current;
+
+    if (element) {
+      element.addEventListener("mouseenter", onMouseEnter);
+      element.addEventListener("mouseleave", onMouseLeave);
+      element.addEventListener("mousemove", onMouseMove);
+    }
+
+    return () => {
+      if (element) {
+        element.removeEventListener("mouseenter", onMouseEnter);
+        element.removeEventListener("mouseleave", onMouseLeave);
+        element.removeEventListener("mousemove", onMouseMove);
+      }
+    };
+  }, [elementRef, onMouseEnter, onMouseLeave, onMouseMove]);
+
+  if (!showTooltip) {
+    return null;
+  }
+
+  const renderedClassName = renderClassName(mouseTrack, position);
+
+  return (
+    <>
+      {createPortal(
+        <div
+          ref={tooltipReference}
+          className={className ? className : renderedClassName}
+          style={{
+            top: tooltipPosition.top,
+            left: tooltipPosition.left,
+            ...style,
+          }}
+        >
+          {children}
+        </div>,
+        document.body,
+      )}
+    </>
+  );
+};
