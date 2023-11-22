9807b3e97e59bd38826ee3f63ae90203de980264
Manish Aryal <manish.aryal@dzangolab.com>
2023-11-22 17:04:08 +0545

    feat(input): add additional props for input element

--- packages/ui/src/Input/index.tsx
+++ packages/ui/src/Input/index.tsx
@@ -1,13 +1,31 @@
 import { HTMLAttributes } from "react";
 
 interface IInputProperties extends HTMLAttributes<HTMLInputElement> {
+  type: string;
+  label?: string;
   name?: string;
 }
 
-const Input = ({ name, ...others }: IInputProperties) => {
+const Input = ({
+  type,
+  label,
+  name,
+  placeholder,
+  onChange,
+  ...others
+}: IInputProperties) => {
   return (
-    <div className={`field ${name}`}>
-      <input {...others} />
+    <div className={`input-field ${name}`}>
+      {label && <label htmlFor={name}>{label}</label>}
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        {...others}
+      />
+      {}
     </div>
   );
 };
