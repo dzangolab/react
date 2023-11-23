219d0fa7aaa58c74922e1b6e600146f0aac70a86
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-11-23 15:31:33 +0545

    feat(users-table): add confirmation for both actions

--- packages/user/src/components/UsersTable/UserActions.tsx
+++ packages/user/src/components/UsersTable/UserActions.tsx
@@ -1,20 +1,55 @@
 import { useTranslation } from "@dzangolab/react-i18n";
-import { ActionsMenu } from "@dzangolab/react-ui";
+import { ActionsMenu, ConfirmationModal } from "@dzangolab/react-ui";
 import { MenuItem } from "primereact/menuitem";
+import { useState } from "react";
 
 export const UserAction = () => {
   const { t } = useTranslation("users");
+  const [showEnableConfirmation, setShowEnableConfirmation] = useState(false);
+  const [showDisableConfirmation, setShowDisableConfirmation] = useState(false);
 
   const actionItems: MenuItem[] = [
     {
       label: t("table.actions.enableUser"),
       icon: "pi pi-check",
+      command: () => {
+        setShowEnableConfirmation(true);
+      },
     },
     {
       label: t("table.actions.disableUser"),
       icon: "pi pi-times",
+      command: () => {
+        setShowDisableConfirmation(true);
+      },
     },
   ];
 
-  return <ActionsMenu actions={actionItems} />;
+  return (
+    <>
+      <ActionsMenu actions={actionItems} />
+      <ConfirmationModal
+        visible={showEnableConfirmation}
+        message="Are you sure"
+        header={t("table.actions.enableUser")}
+        onHide={() => {
+          setShowEnableConfirmation(false);
+        }}
+        accept={() => {
+          console.log("enabled");
+        }}
+      />
+      <ConfirmationModal
+        visible={showDisableConfirmation}
+        message="Are you sure"
+        header={t("table.actions.disableUser")}
+        onHide={() => {
+          setShowDisableConfirmation(false);
+        }}
+        accept={() => {
+          console.log("enabled");
+        }}
+      />
+    </>
+  );
 };
