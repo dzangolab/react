eedfff0349a207d4e1aa4dedeb2f1b11142c83af
Manish Aryal <manish.aryal@dzangolab.com>
2023-11-24 16:24:08 +0545

    feat(loginSignUp): add new signup as a component

---
+++ packages/user/src/components/SignUp.tsx
@@ -0,0 +1,52 @@
+import { useTranslation } from "@dzangolab/react-i18n";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+
+import SignupForm from "../components/SignupForm";
+import { useUser } from "../hooks";
+import signup from "../supertokens/signup";
+
+import type { LoginCredentials, SignInUpPromise } from "../types";
+
+interface IProperties {
+  onSignupFailed?: (error: Error) => void;
+  onSignupSuccess?: (user: SignInUpPromise) => void;
+}
+
+const Signup: React.FC<IProperties> = ({ onSignupFailed, onSignupSuccess }) => {
+  const { t } = useTranslation("user");
+  const [loading, setLoading] = useState<boolean>(false);
+  const { setUser } = useUser();
+
+  const handleSubmit = async (credentials: LoginCredentials) => {
+    setLoading(true);
+
+    await signup(credentials)
+      .then(async (result) => {
+        if (result?.user) {
+          await setUser(result.user);
+          onSignupSuccess && (await onSignupSuccess(result));
+
+          toast.success(`${t("signup.messages.success")}`);
+        }
+      })
+      .catch(async (error) => {
+        const errorMessage = t("errors.otherErrors", { ns: "errors" });
+
+        onSignupFailed && (await onSignupFailed(error));
+
+        if (error.name) {
+          throw error as Error;
+        }
+
+        toast.error(error.message || errorMessage);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return <SignupForm handleSubmit={handleSubmit} loading={loading} />;
+};
+
+export default Signup;
