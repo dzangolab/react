9b51a37224dd98d30369952117651ffbca2f13c6
Manish Aryal <manish.aryal@dzangolab.com>
2023-11-28 17:33:25 +0545

    feat(loginsignup): add type import for react node

--- packages/user/src/components/SignupBasic.tsx
+++ packages/user/src/components/SignupBasic.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from "@dzangolab/react-i18n";
-import React, { useState } from "react";
+import React, { ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
 import { toast } from "react-toastify";
 
