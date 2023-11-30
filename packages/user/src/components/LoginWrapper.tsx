7402d92c23d3a8d41ef33f0d2b9b2b0c0565a70e
Manish Aryal <manish.aryal@dzangolab.com>
2023-11-30 11:24:11 +0545

    feat(loginSignup): add login and sign in wrapper components

---
+++ packages/user/src/components/LoginWrapper.tsx
@@ -0,0 +1,56 @@
+import { useTranslation } from "@dzangolab/react-i18n";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+import LoginForm from "./LoginForm";
+import { useConfig, useUser } from "../hooks";
+import { verifySessionRoles } from "../supertokens/helpers";
+import login from "../supertokens/login";
+
+import type { LoginCredentials, SignInUpPromise } from "../types";
+
+const LoginWrapper = () => {
+  const { t } = useTranslation(["user", "errors"]);
+  const { setUser } = useUser();
+  const appConfig = useConfig();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async (credentials: LoginCredentials) => {
+    setLoading(true);
+
+    await login(credentials)
+      .then(async (result) => {
+        if (result?.user) {
+          if (
+            appConfig &&
+            (await verifySessionRoles(appConfig.user.supportedRoles))
+          ) {
+            setUser(result.user);
+
+            // onLoginSuccess && (await onLoginSuccess(result));
+
+            toast.success(`${t("login.messages.success")}`);
+          } else {
+            toast.error(t("login.messages.permissionDenied"));
+          }
+        }
+      })
+      .catch(async (error) => {
+        let errorMessage = "errors.otherErrors";
+
+        if (error.message) {
+          errorMessage = `errors.${error.message}`;
+        }
+
+        // onLoginFailed && (await onLoginFailed(error));
+
+        toast.error(t(errorMessage, { ns: "errors" }));
+      });
+
+    setLoading(false);
+  };
+
+  return <LoginForm handleSubmit={handleSubmit} loading={loading} />;
+};
+
+export default LoginWrapper;
