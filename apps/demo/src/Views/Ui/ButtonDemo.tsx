7c37ec4d73e8d6dbdfc0c643f34ffdd1f943e4f7
Manish Aryal <manish.aryal@dzangolab.com>
2023-12-13 14:50:05 +0545

    feat(button): add different padding for sizes

--- apps/demo/src/Views/Ui/ButtonDemo.tsx
+++ apps/demo/src/Views/Ui/ButtonDemo.tsx
@@ -3,9 +3,25 @@ import { Button } from "@dzangolab/react-ui";
 export const ButtonDemo = () => {
   return (
     <div className="buttons-wrapper">
-      <Button variant="filled" severity="secondary" loading={true} />
-      <Button variant="outlined" severity="secondary" />
-      <Button severity="alternate" variant="outlined" loading={true} />
+      <Button
+        variant="filled"
+        severity="secondary"
+        loading={true}
+        loadingIcon={<i className="pi pi-spin pi-spinner" />}
+      />
+      <Button
+        size="small"
+        variant="outlined"
+        severity="secondary"
+        loading={true}
+        loadingIcon={<i className="pi pi-spin pi-spinner" />}
+      />
+      <Button
+        severity="alternate"
+        variant="outlined"
+        loading={true}
+        loadingIcon={<i className="pi pi-spin pi-spinner" />}
+      />
       <Button variant="filled" disabled={true} />
     </div>
   );
--- packages/ui/src/Buttons/ButtonBasic/index.tsx
+++ packages/ui/src/Buttons/ButtonBasic/index.tsx
@@ -1,13 +1,12 @@
 import { ButtonHTMLAttributes, FC, ReactNode } from "react";
 
-import LoadingIcon from "../../LoadingIcon";
-
 interface IButtonProperties extends ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
   severity?: "primary" | "secondary" | "alternate";
   variant?: "outlined" | "filled";
   icon?: string | ReactNode;
   loading?: boolean;
+  loadingIcon?: string | ReactNode;
   iconPosition?: "left" | "right";
   size?: "small" | "medium" | "large";
 }
@@ -18,6 +17,7 @@ export const Button: FC<IButtonProperties> = ({
   variant = "filled",
   icon,
   loading,
+  loadingIcon,
   disabled,
   iconPosition = "left",
   size = "medium",
@@ -25,17 +25,14 @@ export const Button: FC<IButtonProperties> = ({
   ...otherProperties
 }) => {
   const renderIcon = () => {
-    return (
-      <span className="button-icon">
-        {loading ? (
-          <LoadingIcon />
-        ) : typeof icon === "string" ? (
-          <i className={icon} />
-        ) : (
-          icon
-        )}
-      </span>
-    );
+    if (loading && loadingIcon) {
+      if (typeof loadingIcon === "string") <i className={loadingIcon} />;
+      return loadingIcon;
+    }
+
+    if (icon && typeof icon === "string") <i className={icon} />;
+
+    return icon;
   };
 
   return (
@@ -45,7 +42,7 @@ export const Button: FC<IButtonProperties> = ({
       className={`button ${severity} ${variant} ${iconPosition} ${size}`}
       {...otherProperties}
     >
-      {renderIcon()}
+      <span className="button-icon">{renderIcon()}</span>
       <span className="button-label">{label}</span>
     </button>
   );
