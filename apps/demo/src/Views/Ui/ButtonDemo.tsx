98040c00cf4f475d28e4e52c6b667bd7bde00871
Manish Aryal <manish.aryal@dzangolab.com>
2023-12-14 09:51:09 +0545

    feat(button): improve label for button label

--- apps/demo/src/Views/Ui/ButtonDemo.tsx
+++ apps/demo/src/Views/Ui/ButtonDemo.tsx
@@ -7,27 +7,31 @@ export const ButtonDemo = () => {
   return (
     <div className="buttons-wrapper">
       <Button
-        label={t("button.label")}
+        label={t("button.label.secondary")}
         size="small"
         variant="filled"
         severity="secondary"
         icon="pi pi-check"
       />
       <Button
-        label={t("button.label")}
+        label={t("button.label.secondary")}
         size="medium"
         variant="outlined"
         severity="secondary"
       />
       <Button
-        label={t("button.label")}
+        label={t("button.label.alternate")}
         size="large"
         severity="alternate"
         variant="outlined"
         loading={true}
         loadingIcon="pi pi-spin pi-spinner"
       />
-      <Button label={t("button.label")} variant="filled" icon="pi pi-user" />
+      <Button
+        label={t("button.label.primary")}
+        variant="filled"
+        icon="pi pi-user"
+      />
       <Button
         variant="filled"
         severity="alternate"
