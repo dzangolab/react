56ed0f1045660263d452b291662124e028a1bd21
Manish Aryal <manish.aryal@dzangolab.com>
2023-12-15 15:27:51 +0545

    refactor(button): add button for the popup button

--- apps/demo/src/Views/Ui/PopupDemo.tsx
+++ apps/demo/src/Views/Ui/PopupDemo.tsx
@@ -1,13 +1,12 @@
 import { useTranslation } from "@dzangolab/react-i18n";
-import { Popup } from "@dzangolab/react-ui";
-import { Button } from "primereact/button";
+import { Popup, Button } from "@dzangolab/react-ui";
 
 const PopupDemo = () => {
   const { t } = useTranslation("ui");
 
   return (
     <Popup
-      trigger={<Button icon={"pi pi-angle-double-down"}></Button>}
+      trigger={<Button iconLeft={"pi pi-angle-double-down"}></Button>}
       content={<div style={{ padding: "1rem" }}>{t("popup.content")}</div>}
       position="right"
       offset={20}
