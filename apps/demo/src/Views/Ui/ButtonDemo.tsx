6b5e69ccaabfe59e9284f619c4e688eb45455a29
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2023-12-15 13:52:07 +0545

    feat(button-demo): add more buttons in demo

--- apps/demo/src/Views/Ui/ButtonDemo.tsx
+++ apps/demo/src/Views/Ui/ButtonDemo.tsx
@@ -6,38 +6,89 @@ export const ButtonDemo = () => {
 
   return (
     <div className="buttons-wrapper">
-      <Button
-        label={t("button.label.secondary")}
-        size="small"
-        variant="filled"
-        severity="secondary"
-        iconLeft="pi pi-check"
-      />
-      <Button
-        label={t("button.label.secondary")}
-        size="medium"
-        variant="outlined"
-        severity="secondary"
-      />
-      <Button
-        label={t("button.label.alternate")}
-        size="large"
-        severity="alternate"
-        variant="outlined"
-        loading={true}
-        iconLeft="pi pi-spin pi-spinner"
-      />
-      <Button
-        label={t("button.label.primary")}
-        variant="filled"
-        iconRight="pi pi-user"
-      />
-      <Button
-        variant="filled"
-        severity="alternate"
-        disabled={true}
-        iconLeft="pi pi-user"
-      />
+      <h3>Severity</h3>
+      <div className="button-severity">
+        <Button label={t("button.label.primary")} severity="primary" />
+        <Button label={t("button.label.secondary")} severity="secondary" />
+        <Button label={t("button.label.alternate")} severity="alternate" />
+      </div>
+      <h3>Sizes</h3>
+      <div className="button-size">
+        <Button
+          label="small button"
+          size="small"
+          variant="filled"
+          severity="secondary"
+          iconLeft="pi pi-check"
+        />
+        <Button
+          label="medium button"
+          size="medium"
+          variant="filled"
+          severity="secondary"
+          iconLeft="pi pi-check"
+        />
+        <Button
+          label="large button"
+          size="large"
+          variant="filled"
+          severity="secondary"
+          iconLeft="pi pi-check"
+        />
+      </div>
+      <h3>Outlined</h3>
+      <div className="button-outline">
+        <Button
+          label={t("button.label.primary")}
+          size="medium"
+          variant="outlined"
+          severity="primary"
+        />
+        <Button
+          label={t("button.label.secondary")}
+          size="medium"
+          variant="outlined"
+          severity="secondary"
+        />
+        <Button
+          label={t("button.label.alternate")}
+          size="medium"
+          severity="alternate"
+          variant="outlined"
+        />
+      </div>
+      <h3>Icons</h3>
+      <div className="button-icon">
+        <Button
+          label={t("button.label.alternate")}
+          size="medium"
+          severity="alternate"
+          variant="outlined"
+          loading={true}
+          iconLeft="pi pi-spin pi-spinner"
+        />
+        <Button
+          label={t("button.label.primary")}
+          variant="filled"
+          iconRight="pi pi-user"
+        />
+        <Button
+          label={t("button.label.secondary")}
+          variant="filled"
+          severity="secondary"
+          iconLeft="pi pi-user"
+        />
+        <Button variant="filled" severity="alternate" iconLeft="pi pi-user" />
+      </div>
+      <h3>Disabled</h3>
+      <div>
+        <Button
+          label="Disabled"
+          variant="filled"
+          severity="primary"
+          disabled={true}
+        />
+      </div>
     </div>
   );
 };
