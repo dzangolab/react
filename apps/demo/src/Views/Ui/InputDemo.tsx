ade9c8c82666dcfaf54bd8e1cd0deede76acfd4b
Manish Aryal <manish.aryal@dzangolab.com>
2023-12-20 14:01:15 +0545

    feat(input): remove onchange event from the input demo

--- apps/demo/src/Views/Ui/InputDemo.tsx
+++ apps/demo/src/Views/Ui/InputDemo.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { Input } from "@dzangolab/react-ui";
-import { ChangeEvent } from "react";
 
 const InputDemo = () => {
   const { t } = useTranslation("ui");
@@ -9,9 +8,6 @@ const InputDemo = () => {
     <Input
       hasError={true}
       errorMessage={t("input.errorMessage")}
-      onChange={(event: ChangeEvent<HTMLInputElement>) =>
-        console.log(event.target.value)
-      }
       placeholder={t("input.placeholder")}
       label={t("input.label")}
     />
