ee90f3e086f55fa6584a1138f1d3096a22fd274a
Manish Aryal <manish.aryal@dzangolab.com>
2024-01-09 15:56:38 +0545

    feat(subMenu): improve css for the submenu hover and active state

--- packages/layout/src/components/MainMenu.tsx
+++ packages/layout/src/components/MainMenu.tsx
@@ -27,7 +27,7 @@ const MainMenu = (properties: Properties) => {
   const { routes = DEFAULT_ROUTES, orientation = "horizontal" } = properties;
 
   const getTranslatedRoutes = () => {
-    return routes.map(({ name, route }: any) => {
+    return routes.map(({ name, route }) => {
       return {
         name: t(name),
         route,
