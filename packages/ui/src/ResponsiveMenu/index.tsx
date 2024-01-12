d7eb584d3223f939d59675b71f0aafa85719d1b5
manisharyal <manish.aryal@dzangolab.com>
2024-01-12 16:31:50 +0545

    feat(submenu): add support for submenu in sidebar (#712)
    * feat(subMenu): add submenu for sidebar items
    
    * fix(buttonConsistency): add submenu as a component
    
    * feat(subMenu): change logic for toggling sub menu
    
    * feat(subMenu): add styles for sub menu
    
    * feat(subMenu): make submenu dissapear when the aria-expanded is false
    
    * feat(subMenu): change the transition property
    
    * feat(subMenu): add submenu props for the header element
    
    * feat(submenu): add separate property in each route element to show submenu
    
    * feat(subMenu): add transition property for sub menu
    
    * feat(subMenu): remove unnecessary codes from the component
    
    * feat(subMenu): add active classes for the submenu
    
    * feat(subMenu): add transition property for the submenu
    
    * feat(subMenu): add transition for the submenu property
    
    * feat(subMenu): refactor code to separate state updates
    
    * feat(subMenu): add separate component for responsive menu item
    
    * feat(subMenu): change transition property for the submenu item
    
    * feat(subMenu): fix type issues for the menu item
    
    * feat(subMenu): improve css for the submenu hover and active state
    
    * feat(subMenu): change the order of css for submenu
    
    * feat(subMenu): improve name for menu route type
    
    * feat(subMenu): add key for the item element
    
    * feat(subMenu): change the type for submenu and improve css order
    
    * feat(subMenu): change the type and remove unnecessary props
    
    * feat(submenu): change the submenu name to make it more uniform
    
    * feat(submemnu): change submenu prop type to make it more uniform
    
    * feat(submenu): change export type for submenu to default
    
    * feat(submenu): add full path for submenu import
    
    * feat(subMenu): add absolute path for the submenu
    
    * feat(submenu): change the import for submenu in item component
    
    * feat(subMenu): change the name for the folder to solve case issue
    
    * Delete packages/ui/src/assets/css/subMenu.css
    
    * Delete packages/ui/src/SubMenu/index.tsx
    
    * feat(subMenu): add condition before onclick on nav list
    
    * feat(submenu): add separate component for responsive menu group
    
    * feat(submenu): change the name for menu group component
    
    * feat(submenu): change display property for aria expanded false
    
    * feat(submenu): add script to update snapshot and update snapshot
    
    * feat(submenu): improve import statement for the MenuItem
    
    * feat(subMenu): add function inside of useCallback for optimization
    
    * feat(submenu): update snapshot for layout package
    
    * feat(submenu): remove unnecessary imports
    
    * feat(submenu): change the name of the class name after adding dz prefix
    
    * feat(submenu): add key for menu item
    
    * feat(subMenu): change prefix for class name
    
    * feat(subMenu): add animation for the submenu drop
    
    * feat(submenu): update layout snapshots
    
    * feat(submenu): remove isNavActive util function
    
    * feat(submenu): change the style for the visibility of submenu
    
    * feat(submenu): change the type name to nestedmenuroute

--- packages/ui/src/ResponsiveMenu/index.tsx
+++ packages/ui/src/ResponsiveMenu/index.tsx
@@ -1,14 +1,24 @@
-import React, { useCallback } from "react";
-import { NavLink, useInRouterContext } from "react-router-dom";
+import React from "react";
+
+import { MenuItem } from "./Item";
+
+export type MenuRouteType = {
+  name: string;
+  route: string;
+  icon?: React.ReactNode;
+};
+
+export type NestedMenuRouteType = Omit<MenuRouteType, "route"> & {
+  route?: string;
+  submenu: Array<MenuRouteType>;
+};
+
+export type CombinedMenuRouteType = Array<MenuRouteType | NestedMenuRouteType>;
 
 interface Properties {
   className: string;
   orientation?: "horizontal" | "vertical";
-  routes: {
-    name: string;
-    route: string;
-    icon?: React.ReactNode;
-  }[];
+  routes: CombinedMenuRouteType;
   displayIcon?: boolean;
 }
 
@@ -18,66 +28,23 @@ const ResponsiveMenu = ({
   orientation = "horizontal",
   routes,
 }: Properties) => {
-  const hasRouterContext = useInRouterContext();
-
-  let _className = "responsive-menu";
+  let _className = "dz-responsive-menu";
 
   if (className) {
     _className += " " + className;
   }
 
-  const getAnchorList = useCallback(() => {
-    const checkIsActive = (link: string) => {
-      const pathnameArray = window.location.pathname.split("/");
-      const isActive =
-        window.location.pathname.startsWith(link) ||
-        (pathnameArray.length && pathnameArray.includes(link));
-
-      return isActive;
-    };
-
+  const renderMenu = (routes: CombinedMenuRouteType, displayIcon: boolean) => {
     return routes.map((route) => {
-      const isActive = checkIsActive(route.route);
-
       return (
-        <li key={route.name}>
-          <a
-            href={route.route}
-            className={isActive ? "active" : undefined}
-            aria-current={isActive ? "page" : undefined}
-          >
-            {displayIcon ? (
-              <span role="icon" title={route.name}>
-                {route.icon}
-              </span>
-            ) : null}
-            <span role="label">{route.name}</span>
-          </a>
-        </li>
+        <MenuItem key={route.name} route={route} displayIcon={displayIcon} />
       );
     });
-  }, [routes]);
-
-  const getRouterList = useCallback(
-    () =>
-      routes.map((route) => (
-        <li key={route.name}>
-          <NavLink to={route.route} end={route.route === "/"}>
-            {displayIcon ? (
-              <span role="icon" title={route.name}>
-                {route.icon}
-              </span>
-            ) : null}
-            <span role="label">{route.name}</span>
-          </NavLink>
-        </li>
-      )),
-    [routes],
-  );
+  };
 
   return (
     <nav className={_className} aria-orientation={orientation}>
-      <ul>{hasRouterContext ? getRouterList() : getAnchorList()}</ul>
+      <ul>{renderMenu(routes, displayIcon)}</ul>
     </nav>
   );
 };
