ba5c57b1cf6a3aa3a6424f49d5e23d5af516ad2f
Manish Aryal <manish.aryal@dzangolab.com>
2024-01-17 14:33:25 +0545

    feat(subHeadingTag): add the tag in the demo application

--- apps/demo/src/Views/Ui/components/PageDemo.tsx
+++ apps/demo/src/Views/Ui/components/PageDemo.tsx
@@ -49,6 +49,7 @@ export const PageDemo = () => {
       <hr />
       <Page
         title={t("page.title.complete")}
+        headingSubtitle={<Tag value={t("page.headingSubtitle.tag")} />}
         toolbar={<Button label={t("page.toolbar.button.label")} />}
         subtitle={<Tag value={t("page.subTitle.tag")} />}
         children={PageContent}
