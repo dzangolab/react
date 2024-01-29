914c05a5fb0249c644d32b73bd5a8b546c934c1c
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-01-29 15:14:11 +0545

    feat(table): remove div from table header

--- packages/ui/src/Table/TableElements.tsx
+++ packages/ui/src/Table/TableElements.tsx
@@ -56,7 +56,7 @@ const ColumnHeader = React.forwardRef<
   <th
     className={className || ""}
     ref={reference}
-    children={<div className="content"> {children}</div>}
+    children={children}
     {...properties}
   />
 ));
