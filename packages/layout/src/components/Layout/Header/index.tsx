35922025a9326d8032eda1f1708333a8d37b80c6
suvashvishowkarma <80803740+suvashvishowkarma@users.noreply.github.com>
2024-02-07 17:09:22 +0545

    feat(layout): add support for nav group (#805)

--- packages/layout/src/components/Layout/Header/index.tsx
+++ packages/layout/src/components/Layout/Header/index.tsx
@@ -6,12 +6,14 @@ import { NavMenuType } from "../types";
 
 interface HeaderProperties {
   children?: React.ReactNode;
+  displayNavIcons?: boolean;
   navigationMenu?: NavMenuType;
   userMenu?: React.ReactNode; // TODO update to NavigationMenuItem[] when we have UserMenu component in layout package
 }
 
 export const Header = ({
   children,
+  displayNavIcons,
   navigationMenu,
   userMenu,
 }: HeaderProperties) => {
@@ -19,9 +21,11 @@ export const Header = ({
     return (
       <>
         <Logo />
-        {navigationMenu && (
-          <NavigationMenu navigationMenu={navigationMenu} horizontal />
-        )}
+        <NavigationMenu
+          navigationMenu={navigationMenu || []}
+          displayIcons={displayNavIcons}
+          horizontal
+        />
         {userMenu}
         <LocaleSwitcher />
         <ToggleMenuMobile />
