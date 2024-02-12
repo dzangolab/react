bdd13e5cc1fc77d57770397a8374b58ed843de42
lamdiki sherpa <lamdiki.sherpa@dzangolab.com>
2024-02-12 13:42:05 +0545

    refactor(form/input): improve read-only display (#813)
    * feat(input-field): add css for readonly input field
    
    * feat(input-field): update css selector
    
    * feat(input-field): add padding for input field
    
    * feat(input-field): change props name

--- packages/form/src/components/Email.tsx
+++ packages/form/src/components/Email.tsx
@@ -6,6 +6,7 @@ import { CustomInputProperties } from "../types";
 export const Email: React.FC<
   CustomInputProperties & { readOnly?: boolean }
 > = ({
+  disabled = false,
   register,
   getFieldState,
   label = "",
@@ -28,6 +29,7 @@ export const Email: React.FC<
         placeholder={placeholder}
         aria-invalid={submitcount > 0 ? invalid : undefined}
         readOnly={readOnly}
+        disabled={disabled}
       ></input>
       {error?.message && <ErrorMessage message={error.message} />}
     </div>
