5ffb892712b1c95b4273cb65076db04e26d2e59d
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-03-07 13:22:33 +0545

    feat(number-input): add zod validation

--- apps/demo/src/Views/Form/components/FileInput/FormInput/FormInput.tsx
+++ apps/demo/src/Views/Form/components/FileInput/FormInput/FormInput.tsx
@@ -27,9 +27,14 @@ export const FormInputDemo = () => {
     surname: zod.string().nonempty({
       message: t("formInput.message.required"),
     }),
-    number: zod.number({
-      required_error: t("formInput.message.required"),
-    }),
+    number: zod
+      .number({
+        required_error: t("formInput.message.required"),
+      })
+      .nullable()
+      .refine((data) => data !== null, {
+        message: t("formInput.message.required"),
+      }),
   });
 
   const handleSubmit = (formData: any) => {
