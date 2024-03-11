e43f9eca8c391573f7c6fa541f1990cea0a1d98b
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-03-11 11:35:20 +0545

    feat(form-input): add form valid and invalid mode

--- apps/demo/src/Views/Form/components/FileInput/FormInput/FormInputFields.tsx
+++ apps/demo/src/Views/Form/components/FileInput/FormInput/FormInputFields.tsx
@@ -8,36 +8,43 @@ import {
   NumberInput,
 } from "@dzangolab/react-form";
 import { useTranslation } from "@dzangolab/react-i18n";
+import { useState } from "react";
 
 export const FormInputFields = () => {
   const [t] = useTranslation("form");
-
+  const [valid, setValid] = useState(false);
+  const [invalid, setInvalid] = useState(false);
   const {
     register,
     getFieldState,
     formState: { errors, submitCount },
   } = useFormContext();
-
   return (
     <>
       <Email
         label={t("formInput.label.email")}
         name="email"
         placeholder={t("formInput.placeHolder.email")}
-        submitcount={submitCount}
+        submitcount={valid || invalid ? 1 : submitCount}
+        showValidState={valid}
+        showInvalidState={invalid}
       />
       <TextInput
         label={t("formInput.label.text")}
         name="name"
         placeholder={t("formInput.placeHolder.text")}
-        submitcount={submitCount}
+        submitcount={valid || invalid ? 1 : submitCount}
+        showValidState={valid}
+        showInvalidState={invalid}
       />
       <Password
         getFieldState={getFieldState}
         label={t("formInput.label.password")}
         name="password"
         register={register}
-        submitcount={submitCount}
+        submitcount={valid || invalid ? 1 : submitCount}
+        showValidState={valid}
+        showInvalidState={invalid}
       />
       <Input
         getFieldState={getFieldState}
@@ -45,14 +52,18 @@ export const FormInputFields = () => {
         name="surname"
         placeholder={t("formInput.placeHolder.input")}
         register={register}
-        submitcount={submitCount}
+        submitcount={valid || invalid ? 1 : submitCount}
         type="text"
+        showValidState={valid}
+        showInvalidState={invalid}
       />
       <NumberInput
         name="number"
         label={t("formInput.label.number")}
         placeholder={t("formInput.placeHolder.number")}
-        submitcount={submitCount}
+        submitcount={valid || invalid ? 1 : submitCount}
+        showInvalidState={invalid}
+        showValidState={valid}
       />
       <FormActions
         actions={[
@@ -66,6 +77,23 @@ export const FormInputFields = () => {
           {
             id: "submit",
             label: t("formInput.action.submit"),
+          },
+          {
+            id: "valid",
+            label: "valid state",
+            onClick: (event) => {
+              event.preventDefault();
+              setValid(true);
+              setInvalid(false);
+            },
+          },
+          {
+            id: "Invalid",
+            label: "Invalid state",
+            onClick: () => {
+              setInvalid(true);
+              setValid(false);
+            },
           },
         ]}
         alignment="left"
