f9029a3f26bb9cb0693bb3b40ccd515db33f7b69
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-03-26 13:55:48 +0545

    feat(select): render select options

--- packages/ui/src/Select/index.tsx
+++ packages/ui/src/Select/index.tsx
@@ -2,33 +2,58 @@ import { useState } from "react";
 
 interface ISelect {
   label?: string;
+  multiple?: boolean;
   name: string;
   options: { value: string; label: string }[];
+  value: string[];
+  onChange: (newValue: string[]) => void;
 }
 
-export const Select = ({ label = "", name, options }: ISelect) => {
+export const Select = ({
+  label = "",
+  multiple,
+  name,
+  options,
+  value,
+  onChange,
+}: ISelect) => {
   const [showOptions, setShowOptions] = useState(false);
+
+  const handleSelectedOption = (option: string) => {
+    if (!multiple) {
+      onChange([option]);
+    } else {
+      const newValue = value.includes(option) ? value : [...value, option];
+      onChange(newValue);
+    }
+  };
 
   return (
     <div className={`select-input ${name}`}>
       {label && <label htmlFor={name}>{label}</label>}
       <div className="select">
-        <input type="text" readOnly />
+        <input type="text" value={value.join(", ")} readOnly />
         <span onClick={() => setShowOptions(!showOptions)}>
           <i className="pi pi-chevron-down"></i>
         </span>
       </div>
-      <div className={showOptions ? "select-options" : ""}>
-        {showOptions &&
-          options?.map((option, index) => {
+
+      {showOptions && (
+        <div className="select-options">
+          {options?.map((option, index) => {
             const { value, label } = option;
             return (
-              <span key={index} style={{ cursor: "pointer" }}>
+              <span
+                key={index}
+                onClick={() => handleSelectedOption(value)}
+                style={{ cursor: "pointer" }}
+              >
                 {label}
               </span>
             );
           })}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
