0f9c36caa803366bb03ea245de2d0e79219d6b53
Manish Aryal <manish.aryal@dzangolab.com>
2024-04-24 15:05:42 +0545

    feat(layout): add props for layout customization

--- packages/layout/src/layouts/CollapsibleSidebarHeaderLayout.tsx
+++ packages/layout/src/layouts/CollapsibleSidebarHeaderLayout.tsx
@@ -1,23 +1,50 @@
-import { Layout } from "..";
+import { Layout, NavMenuType, Sidebar } from "..";
 
 interface IProperties {
   className?: string;
   children: React.ReactNode;
   collapsible?: boolean;
-  displayHeader?: boolean;
-  displaySidebar?: boolean;
   customHeader?: React.ReactNode;
   customSidebar?: React.ReactNode;
+  displayHeader?: boolean;
+  displayLocaleSwitcher?: boolean;
+  displayNavIcons?: boolean;
+  displaySidebar?: boolean;
+  displaySidebarFooter?: boolean;
+  localeSwitcher?: boolean;
+  navigationMenu?: NavMenuType;
+  userMenu?: React.ReactNode;
 }
 
 const CollapsibleSidebarHeaderLayout = ({
   className,
   children,
+  collapsible,
+  customHeader,
+  customSidebar,
+  displayHeader = true,
+  displayLocaleSwitcher = false,
+  displayNavIcons = true,
+  displaySidebar = true,
+  displaySidebarFooter = false,
+  localeSwitcher,
+  navigationMenu,
+  userMenu,
 }: IProperties) => {
   return (
     <Layout className={`header-sidebar ${className || ""}`.trimEnd()}>
-      <header></header>
-      <aside></aside>
+      {displayHeader && (customHeader || <header></header>)}
+      {displaySidebar &&
+        (customSidebar || (
+          <Sidebar
+            collapsible={collapsible}
+            displayNavIcons={displayNavIcons}
+            navigationMenu={navigationMenu}
+            noHeader={true}
+            noFooter={!displaySidebarFooter}
+            noLocaleSwitcher={!displayLocaleSwitcher}
+          ></Sidebar>
+        ))}
       <main>{children}</main>
     </Layout>
   );
