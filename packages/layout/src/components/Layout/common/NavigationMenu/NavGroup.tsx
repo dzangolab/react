e4332edf460ca44f7e947e079847e3af0a95cd3f
Manish Aryal <manish.aryal@dzangolab.com>
2024-05-14 11:40:00 +0545

    feat(submenu): change the class name conditionally

--- packages/layout/src/components/Layout/common/NavigationMenu/NavGroup.tsx
+++ packages/layout/src/components/Layout/common/NavigationMenu/NavGroup.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@dzangolab/react-ui";
 import { useState } from "react";
 
 import { NavItem } from "./NavItem";
@@ -5,14 +6,17 @@ import { NavGroupType } from "../../types";
 
 export type NavGroupProperties = {
   displayIcon?: boolean;
+  horizontal?: boolean;
   navGroup: NavGroupType;
 };
 
 export const NavGroup = ({
-  navGroup,
   displayIcon = true,
+  horizontal,
+  navGroup,
 }: NavGroupProperties) => {
   const [showSubmenu, setShowSubmenu] = useState(false);
+  const isSmallScreen = useMediaQuery("(max-width:576px)");
 
   const renderSubmenu = () => {
     return (
@@ -29,8 +33,13 @@ export const NavGroup = ({
     );
   };
 
+  const navGroupProperties =
+    isSmallScreen || !horizontal
+      ? { className: "nav-group", "aria-expanded": showSubmenu }
+      : { className: "nav-group" };
+
   return (
-    <div className="nav-group" aria-expanded={showSubmenu}>
+    <div {...navGroupProperties}>
       <NavItem
         navItem={{
           label: navGroup.label,
