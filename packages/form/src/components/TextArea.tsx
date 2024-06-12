d96ef0df618107e0b38edfd08b8947ba4c0ff8b1
Manish Aryal <manish.aryal@dzangolab.com>
2024-06-12 16:53:40 +0545

    feat(form-textarea): add a new textarea component controller

--- packages/form/src/components/TextArea.tsx
+++ packages/form/src/components/TextArea.tsx
@@ -1,53 +1,64 @@
+import { ITextareaProperties } from "@dzangolab/react-ui";
 import React from "react";
-import { UseFormGetFieldState, UseFormRegister } from "react-hook-form";
+import {
+  Controller,
+  UseFormGetFieldState,
+  UseFormRegister,
+  useFormContext,
+} from "react-hook-form";
 
-import { ErrorMessage } from "./ErrorMessage";
-
-interface ITextArea {
-  disabled?: boolean;
+interface ITextArea extends ITextareaProperties {
+  defaultValue?: string;
   label?: string;
   name: string;
   placeholder?: string;
   showValidState?: boolean;
   showInvalidState?: boolean;
   submitCount?: number;
+  /** @deprecated */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getFieldState?: UseFormGetFieldState<any>;
+  /** @deprecated */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   register?: UseFormRegister<any>;
 }
 
 export const TextArea: React.FC<ITextArea> = ({
-  disabled,
-  register,
-  getFieldState,
+  defaultValue = "",
   label = "",
   placeholder = "",
   name,
   showInvalidState = true,
   showValidState = true,
   submitCount = 0,
+  ...others
 }) => {
-  if (!register || !getFieldState) return null;
+  const { control, getFieldState } = useFormContext();
 
   const { error, invalid } = getFieldState(name);
 
   const checkInvalidState = () => {
     if (showInvalidState && invalid) return true;
-
     if (showValidState && !invalid) return false;
   };
 
   return (
-    <div className={`field textarea-input ${name}`}>
-      {label && <label htmlFor={name}>{label}</label>}
-      <textarea
-        {...register(name)}
-        placeholder={placeholder}
-        aria-invalid={submitCount > 0 ? checkInvalidState() : undefined}
-        disabled={disabled}
-      ></textarea>
-      {error?.message && <ErrorMessage message={error.message} />}
-    </div>
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      render={({ field }) => (
+        <TextArea
+          name={field.name}
+          label={label}
+          placeholder={placeholder}
+          defaultValue={field.value}
+          errorMessage={error?.message}
+          onChange={field.onChange}
+          hasError={submitCount > 0 ? checkInvalidState() : undefined}
+          {...others}
+        />
+      )}
+    />
   );
 };
