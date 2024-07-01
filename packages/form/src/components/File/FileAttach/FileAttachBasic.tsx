5fbd7430f7d2760aaadfca566ec0717516d94ae5
shailesh <shailesh@dzangolab.com>
2024-07-01 14:10:06 +0545

    feat(form): remove Overlay support from FileAttach

--- packages/form/src/components/File/FileAttach/FileAttachBasic.tsx
+++ packages/form/src/components/File/FileAttach/FileAttachBasic.tsx
@@ -1,33 +1,23 @@
 // components/FormComponents/FileAttachBasic.tsx
 import { Button } from "@dzangolab/react-ui";
-import { OverlayPanel } from "primereact/overlaypanel";
-import React, { useRef } from "react";
+import React from "react";
 import { useDropzone } from "react-dropzone";
 
-import { useOnDropFile, useOnRemoveFile } from "../hooks";
-import { SelectedFile } from "../SelectedFile";
+import { useOnDropFile } from "../hooks";
 
 import type { IFileAttachBasicProperties } from "../types";
-import type { FC, LegacyRef } from "react";
+import type { FC } from "react";
 
 export const FileAttachBasic: FC<IFileAttachBasicProperties> = ({
   name,
-  selectedFileDisplay = "none",
   mode = "append",
   multiple = true,
   value = [],
   dropzoneOptions,
-  enableDescription = false,
-  emptySelectionMessage = "No file selected.",
-  addDescriptionLabel,
-  descriptionPlaceholder,
   onChange,
   selectButtonProps,
 }) => {
-  const overlayReference = useRef<OverlayPanel>();
-
   const onDrop = useOnDropFile({ mode, name, onChange, value, multiple });
-  const onRemove = useOnRemoveFile({ value, onChange });
 
   const { getRootProps, getInputProps } = useDropzone({
     noDrag: true,
@@ -42,10 +32,6 @@ export const FileAttachBasic: FC<IFileAttachBasicProperties> = ({
     return (
       <div className="input-button-wrapper">
         <Button
-          onMouseEnter={(event) =>
-            selectedFileDisplay === "popup" &&
-            overlayReference.current?.show(event, null)
-          }
           onClick={(event) => {
             event.preventDefault();
             onClick?.(event);
@@ -57,40 +43,5 @@ export const FileAttachBasic: FC<IFileAttachBasicProperties> = ({
     );
   };
 
-  const renderSelectedFiles = () => (
-    <ul className="selected">
-      {!!value?.length &&
-        value.map((file, index) => {
-          return (
-            <SelectedFile
-              key={file.name}
-              file={file}
-              index={index}
-              enableDescription={enableDescription}
-              addDescriptionLabel={addDescriptionLabel}
-              descriptionPlaceholder={descriptionPlaceholder}
-              onRemove={() => onRemove(index)}
-              onDescriptionChange={(description) => {
-                file.description = description;
-              }}
-            />
-          );
-        })}
-    </ul>
-  );
-
-  return (
-    <div className="file-input attach">
-      {renderInputUi()}
-
-      {selectedFileDisplay === "popup" ? (
-        <OverlayPanel
-          ref={overlayReference as LegacyRef<OverlayPanel>}
-          showCloseIcon
-        >
-          {value?.length ? renderSelectedFiles() : emptySelectionMessage}
-        </OverlayPanel>
-      ) : null}
-    </div>
-  );
+  return <div className="file-input attach">{renderInputUi()}</div>;
 };
