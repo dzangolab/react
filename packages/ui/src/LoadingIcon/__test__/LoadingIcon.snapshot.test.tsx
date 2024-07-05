a87675cd5334345aef717c53c19684b2580f16f1
Manish Aryal <manish.aryal@dzangolab.com>
2024-07-05 15:39:39 +0545

    test(ui): update the method for snapshot test

--- packages/ui/src/LoadingIcon/__test__/LoadingIcon.snapshot.test.tsx
+++ packages/ui/src/LoadingIcon/__test__/LoadingIcon.snapshot.test.tsx
@@ -1,20 +1,10 @@
+import { render } from "@testing-library/react";
 import React from "react";
-import renderer, { create } from "react-test-renderer";
 import { expect, test } from "vitest";
 
 import LoadingIcon from "../../LoadingIcon";
 
-function toJson(component: renderer.ReactTestRenderer) {
-  const result = component.toJSON();
-
-  expect(result).toBeDefined();
-  expect(result).not.toBeInstanceOf(Array);
-
-  return result as renderer.ReactTestRendererJSON;
-}
-
 test("Component matches snapshot", () => {
-  const component = create(<LoadingIcon />);
-  const tree = toJson(component);
-  expect(tree).toMatchSnapshot();
+  const { container } = render(<LoadingIcon />);
+  expect(container).toMatchSnapshot();
 });
