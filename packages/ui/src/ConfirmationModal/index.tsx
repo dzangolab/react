3adc3285ad07d47f242e80ce350aa8d6e930592f
manisharyal <manish.aryal@dzangolab.com>
2024-07-05 11:48:56 +0545

    feat(confirmation-modal): replace the prime react dialog with html di… (#1057)
    * feat(confirmation-modal): replace the prime react dialog with html dialog element
    
    * refactor(confirmation-modal): add the css for the content and footer
    
    * refactor(confirmation-modal): fix the border radius issue in closable icon
    
    * refactor(confirmation-modal): change the test yml for github actions

--- packages/ui/src/ConfirmationModal/index.tsx
+++ packages/ui/src/ConfirmationModal/index.tsx
@@ -1,11 +1,21 @@
-import { ConfirmDialog, ConfirmDialogProps } from "primereact/confirmdialog";
+import { DialogHTMLAttributes, ReactNode, useEffect, useRef } from "react";
 
 import { Button, IButtonProperties } from "..";
 
-export interface IModalProperties extends ConfirmDialogProps {
+export interface IModalProperties
+  extends DialogHTMLAttributes<HTMLDialogElement> {
   acceptButtonOptions?: IButtonProperties;
   cancelButtonOptions?: IButtonProperties;
+  closable?: boolean;
+  closeIcon?: string | ReactNode;
+  footer?: ReactNode;
+  header?: ReactNode | string;
+  icon?: ReactNode | string;
+  message?: string;
+  visible?: boolean;
+  accept?: () => void;
   onHide?: () => void;
+  reject?: () => void;
 }
 
 export const ConfirmationModal = ({
@@ -13,18 +23,64 @@ export const ConfirmationModal = ({
   acceptButtonOptions,
   reject,
   cancelButtonOptions,
+  closable = true,
+  closeIcon = "pi pi-times",
+  className = "",
   message,
   header,
+  icon = "pi pi-exclamation-triangle",
   visible,
   onHide,
   footer,
-  ...confirmOptions
+  children,
+  ...dialogOptions
 }: IModalProperties) => {
+  const dialogReference = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (visible) {
+      dialogReference.current?.showModal();
+    } else {
+      dialogReference.current?.close();
+    }
+  }, [visible]);
+
+  const renderHeader = () => {
+    if (!header && !closable) {
+      return null;
+    }
+
+    return (
+      <div className="dz-dialog-header">
+        {typeof header === "string" ? (
+          <span className="title">{header}</span>
+        ) : (
+          header
+        )}
+        {closable && (
+          <Button
+            variant="textOnly"
+            size="small"
+            severity="secondary"
+            iconLeft={
+              typeof closeIcon === "string" ? (
+                <i className={closeIcon} />
+              ) : (
+                closeIcon
+              )
+            }
+            onClick={onHide}
+          />
+        )}
+      </div>
+    );
+  };
+
   const renderFooter = () => {
     if (footer) return footer;
 
     return (
-      <div className="dialog-footer">
+      <div className="dz-dialog-footer">
         <Button
           label="No"
           variant="outlined"
@@ -41,20 +97,30 @@ export const ConfirmationModal = ({
     );
   };
 
-  return (
-    <>
-      <ConfirmDialog
-        className="dz-confirmation-modal"
-        visible={visible}
-        draggable={false}
-        resizable={false}
-        onHide={onHide}
-        message={message}
-        header={header}
-        footer={renderFooter()}
-        icon="pi pi-exclamation-triangle"
-        {...confirmOptions}
-      />
-    </>
-  );
+  const renderContent = () => {
+    return (
+      <p className="dz-dialog-content">
+        {typeof icon === "string" ? (
+          <i className={icon} style={{ fontSize: "2rem" }} />
+        ) : (
+          icon
+        )}
+        {message}
+      </p>
+    );
+  };
+
+  return visible ? (
+    <dialog
+      ref={dialogReference}
+      className={`dz-dialog ${className}`.trimEnd()}
+      onClose={onHide}
+      {...dialogOptions}
+    >
+      {renderHeader()}
+      {renderContent()}
+      {children}
+      {renderFooter()}
+    </dialog>
+  ) : null;
 };
