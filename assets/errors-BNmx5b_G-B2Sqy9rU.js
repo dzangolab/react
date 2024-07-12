c72f8c96508d08a6b100e1d5d4369fae5ba50bc1
suvashvishowkarma <80803740+suvashvishowkarma@users.noreply.github.com>
2024-07-12 05:46:06 +0000

    Deploying to gh-pages from @ dzangolab/react@850266d92061e49f7222147cce794360b15855a6 🚀

---
+++ assets/errors-BNmx5b_G-B2Sqy9rU.js
@@ -0,0 +1,1 @@
+const r={401:"The credentials you input are invalid.",otherErrors:"Oops! Something went wrong."},e={errors:r};export{e as default,r as errors};
---
+++ assets/errors-CPipGl0I-AN5kbmDB.js
@@ -0,0 +1,1 @@
+const r={401:"The credentials you input are invalid (fr).",otherErrors:"Oops! Something went wrong (fr)."},e={errors:r};export{e as default,r as errors};
