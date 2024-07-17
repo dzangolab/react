1d3cc1fb707851621fedb4639a3b2f9a1291d12a
Manish Aryal <manish.aryal@dzangolab.com>
2024-07-17 13:45:17 +0545

    feat(editable-header): wrap the input component inside of forward ref

--- packages/ui/src/EditableTitle/index.tsx
+++ packages/ui/src/EditableTitle/index.tsx
@@ -1,34 +1,66 @@
-import { ChangeEvent, useState } from "react";
+import {
+  ChangeEvent,
+  HTMLAttributes,
+  createElement,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import { Input } from "..";
 
-interface IProperties {
+interface IProperties extends HTMLAttributes<HTMLHeadElement> {
+  titleLevel?: "h1" | "h2" | "h3";
   title: string;
+  onTitleApply?: () => void;
   onTitleChange: (title: string) => void;
 }
 
-export const EditableTitle = ({ title, onTitleChange }: IProperties) => {
+export const EditableTitle = ({
+  titleLevel = "h1",
+  title,
+  onTitleApply,
+  onTitleChange,
+  ...others
+}: IProperties) => {
   const [isEditModeOn, setEditModeOn] = useState<boolean>(false);
+  const inputReference = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (isEditModeOn) {
+      inputReference.current?.focus();
+    }
+  }, [isEditModeOn]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     onTitleChange(event.target.value);
+  };
+
+  const handleClick = () => {
+    setEditModeOn(true);
+  };
+
+  const handleBlur = () => {
+    setEditModeOn(false);
+    onTitleApply?.();
   };
 
   const renderContent = () => {
     if (isEditModeOn) {
       return (
         <Input
+          ref={inputReference}
           value={title}
           onChange={handleChange}
-          onBlur={() => setEditModeOn(false)}
+          onBlur={handleBlur}
         />
       );
     }
 
-    return (
-      <div className="title" onClick={() => setEditModeOn(true)}>
-        {title}
-      </div>
+    return createElement(
+      titleLevel,
+      { className: "title", onClick: handleClick, ...others },
+      title,
     );
   };
 
--- packages/ui/src/FormWidgets/Input/index.tsx
+++ packages/ui/src/FormWidgets/Input/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, forwardRef } from "react";
 
 export interface IInputProperties
   extends InputHTMLAttributes<HTMLInputElement> {
@@ -8,36 +8,42 @@ export interface IInputProperties
   type?: "text" | "number" | "email";
 }
 
-export const Input = ({
-  defaultValue,
-  disabled,
-  errorMessage,
-  hasError,
-  label,
-  name = "",
-  placeholder,
-  readOnly,
-  type,
-  onChange,
-  ...others
-}: IInputProperties) => {
-  return (
-    <div className={`field ${name}`.trimEnd()}>
-      {label && <label htmlFor={name}>{label}</label>}
-      <input
-        id={name}
-        className={`input-field ${name}`}
-        aria-invalid={hasError}
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-        type={type}
-        value={defaultValue}
-        disabled={disabled}
-        readOnly={readOnly}
-        {...others}
-      />
-      {errorMessage && <span className="error-message">{errorMessage}</span>}
-    </div>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, IInputProperties>(
+  (
+    {
+      defaultValue,
+      disabled,
+      errorMessage,
+      hasError,
+      label,
+      name = "",
+      placeholder,
+      readOnly,
+      type,
+      onChange,
+      ...others
+    }: IInputProperties,
+    reference,
+  ) => {
+    return (
+      <div className={`field ${name}`.trimEnd()}>
+        {label && <label htmlFor={name}>{label}</label>}
+        <input
+          ref={reference}
+          id={name}
+          className={`input-field ${name}`}
+          aria-invalid={hasError}
+          name={name}
+          onChange={onChange}
+          placeholder={placeholder}
+          type={type}
+          value={defaultValue}
+          disabled={disabled}
+          readOnly={readOnly}
+          {...others}
+        />
+        {errorMessage && <span className="error-message">{errorMessage}</span>}
+      </div>
+    );
+  },
+);
