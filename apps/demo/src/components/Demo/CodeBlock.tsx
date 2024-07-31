9e62fa7af43b0ac942b175ae0c92f46eae425839
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2024-07-31 10:28:01 +0545

    feat(demo): add copy action to codeBlock (#1093)
    * feat(demo): add copy action to codeBlock
    
    * fix: lint
    
    * fix: object comparision

--- apps/demo/src/components/Demo/CodeBlock.tsx
+++ apps/demo/src/components/Demo/CodeBlock.tsx
@@ -1,4 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import { useTranslation } from "@dzangolab/react-i18n";
+import { Button } from "@dzangolab/react-ui";
+import React, { useEffect, useRef, useState } from "react";
+
+const COPY_STATES = {
+  copy: { icon: "pi pi-copy", title: "codeBlock.copyStates.copy" },
+  copied: { icon: "pi pi-check", title: "codeBlock.copyStates.copied" },
+  error: {
+    icon: "pi pi-exclamation-triangle",
+    title: "codeBlock.copyStates.error",
+  },
+};
 
 export const CodeBlock = ({
   autoFocus = false,
@@ -11,13 +22,36 @@ export const CodeBlock = ({
   exampleCode: string;
   subheader?: React.ReactNode;
 }) => {
+  const [copyState, setCopyState] = useState(COPY_STATES.copy);
   const reference = useRef<HTMLDivElement>(null);
+
+  const [t] = useTranslation("app");
 
   useEffect(() => {
     if (autoFocus && reference.current) {
       reference.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [autoFocus, reference, exampleCode]);
+
+  const handleCopyClick = () => {
+    if (JSON.stringify(copyState) !== JSON.stringify(COPY_STATES.copy)) {
+      return;
+    }
+
+    navigator.clipboard
+      .writeText(exampleCode)
+      .then(() => {
+        setCopyState(COPY_STATES.copied);
+
+        setTimeout(() => setCopyState(COPY_STATES.copy), 5000);
+      })
+      .catch((err) => {
+        setCopyState(COPY_STATES.error);
+
+        // eslint-disable-next-line no-console
+        console.error("Could not copy text: ", err);
+      });
+  };
 
   const renderHeader = () => {
     if (!title && !subheader) return null;
@@ -37,6 +71,16 @@ export const CodeBlock = ({
       <pre>
         <code>{exampleCode}</code>
       </pre>
+
+      <div className="actions">
+        <Button
+          className="copy"
+          iconLeft={copyState.icon}
+          variant="outlined"
+          title={t(copyState.title)}
+          onClick={handleCopyClick}
+        ></Button>
+      </div>
     </div>
   );
 };
