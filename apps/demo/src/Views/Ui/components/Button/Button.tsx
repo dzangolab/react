261a8d0ddc1746e5efdd9dcc03df267a0774907f
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2024-08-01 13:38:49 +0545

    feat(demo): update buttons page (#1094)
    * feat(demo): update buttons page
    
    * feat: add props description

--- apps/demo/src/Views/Ui/components/Button/Button.tsx
+++ apps/demo/src/Views/Ui/components/Button/Button.tsx
@@ -1,155 +1,154 @@
 import { useTranslation } from "@dzangolab/react-i18n";
-import { Button, Page } from "@dzangolab/react-ui";
+import { TDataTable, Page } from "@dzangolab/react-ui";
 
 import "./index.css";
-import { Section } from "../../../../components/Demo";
+
+import { CodeBlock, Section } from "../../../../components/Demo";
+import { Icon, Link, Severity, Size, Variant } from "./components";
+
+const data = [
+  {
+    id: 1,
+    prop: "children",
+    type: "string | ReactNode",
+    description: "The content to be displayed inside the button.",
+  },
+  {
+    id: 2,
+    prop: "className",
+    type: "string",
+    description: "Additional CSS classes to apply to the button.",
+  },
+  {
+    id: 3,
+    prop: "disabled",
+    type: "boolean",
+    description: "If true, the button will be disabled and non-interactive.",
+  },
+  {
+    id: 4,
+    prop: "iconLeft",
+    type: "string | ReactNode",
+    description:
+      "Icon to be displayed on the left side of the button. Can be a string (class name) or a ReactNode.",
+  },
+  {
+    id: 5,
+    prop: "iconRight",
+    type: "string | ReactNode",
+    description:
+      "Icon to be displayed on the right side of the button. Can be a string (class name) or a ReactNode.",
+  },
+  {
+    id: 6,
+    prop: "label",
+    type: "string",
+    description:
+      "Text to be displayed in the button when `children` is not provided.",
+  },
+  {
+    id: 7,
+    prop: "loading",
+    type: "boolean",
+    description:
+      "If true, indicates that the button is in a loading state and disables user interaction.",
+  },
+  {
+    id: 8,
+    prop: "onClick",
+    type: "() => void",
+    description: "Function to be called when the button is clicked.",
+  },
+  {
+    id: 9,
+    prop: "severity",
+    type: '"primary" | "secondary" | "alternate" | "success" | "danger" | "warning"',
+    description:
+      "Defines the styling severity of the button. Defaults to 'primary'.",
+  },
+  {
+    id: 10,
+    prop: "size",
+    type: '"small" | "medium" | "large"',
+    description: "Sets the size of the button. Defaults to 'medium'.",
+  },
+  {
+    id: 11,
+    prop: "to",
+    type: "string",
+    description:
+      "If provided, the button will act as a `Link` to the specified path instead of a regular button.",
+  },
+  {
+    id: 12,
+    prop: "title",
+    type: "string",
+    description: "Tooltip text that appears on hover.",
+  },
+  {
+    id: 13,
+    prop: "variant",
+    type: '"outlined" | "filled" | "textOnly"',
+    description: "Specifies the button variant. Defaults to 'filled'.",
+  },
+];
 
 export const ButtonDemo = () => {
   const { t } = useTranslation("ui");
 
   return (
-    <Page title={t("button.title")}>
-      <Section title={t("button.usage.severity")}>
-        <div className="demo-buttons-container">
-          <Button label="Primary" severity="primary" />
-          <Button label="Secondary" severity="secondary" />
-          <Button label="Alternate" severity="alternate" />
-          <Button label="Success" severity="success" />
-          <Button label="Danger" severity="danger" />
-          <Button label="Warning" severity="warning" />
-        </div>
+    <Page title={t("button.title")} subtitle={t("button.subtitle")}>
+      <Section title={t("headers.usage")}>
+        <p>
+          To use the Button component, simply import it into your React file:
+        </p>
+        <CodeBlock exampleCode="import { Button } from '@dzangolab/react-ui';" />
+
+        <h3>Properties: IButtonProperties</h3>
+        <TDataTable
+          columns={[
+            {
+              accessorKey: "prop",
+              header: "Properties",
+            },
+            {
+              accessorKey: "type",
+              header: "Type",
+            },
+            {
+              accessorKey: "description",
+              header: "Description",
+            },
+          ]}
+          data={data}
+          paginated={false}
+        />
       </Section>
 
-      <Section title={t("button.usage.size")}>
-        <div className="demo-buttons-container">
-          <Button
-            label="Small"
-            size="small"
-            variant="filled"
-            severity="primary"
-          />
-          <Button
-            label="Medium"
-            size="medium"
-            variant="filled"
-            severity="primary"
-          />
-          <Button
-            label="Large"
-            size="large"
-            variant="filled"
-            severity="primary"
-          />
-        </div>
-      </Section>
+      <Section title={t("headers.examples")}>
+        <h3>{t("button.usage.severity")}</h3>
+        <Severity />
+        <CodeBlock
+          exampleCode={Severity({ isString: true }) as string}
+        ></CodeBlock>
 
-      <Section title={t("button.usage.outlined")}>
-        <div className="demo-buttons-container">
-          <Button
-            label="Primary"
-            size="medium"
-            variant="outlined"
-            severity="primary"
-          />
-          <Button
-            label="Secondary"
-            size="medium"
-            variant="outlined"
-            severity="secondary"
-          />
-          <Button
-            label="Alternate"
-            size="medium"
-            severity="alternate"
-            variant="outlined"
-          />
-          <Button label="Success" severity="success" variant="outlined" />
-          <Button label="Danger" severity="danger" variant="outlined" />
-          <Button label="Warning" severity="warning" variant="outlined" />
-        </div>
-      </Section>
+        <h3>{t("button.usage.size")}</h3>
+        <Size />
+        <CodeBlock exampleCode={Size({ isString: true }) as string}></CodeBlock>
 
-      <Section title={t("button.usage.text")}>
-        <div className="demo-buttons-container">
-          <Button label="Primary" variant="textOnly" />
-          <Button label="Secondary" severity="secondary" variant="textOnly" />
-          <Button label="Alternate" severity="alternate" variant="textOnly" />
-          <Button label="Success" severity="success" variant="textOnly" />
-          <Button label="Danger" severity="danger" variant="textOnly" />
-          <Button label="Warning" severity="warning" variant="textOnly" />
-        </div>
-      </Section>
+        <h3>{t("button.usage.variant")}</h3>
+        <Variant />
+        <CodeBlock
+          exampleCode={Variant({ isString: true }) as string}
+        ></CodeBlock>
 
-      <Section title={t("button.usage.icons")}>
-        <div className="demo-buttons-container">
-          <Button
-            variant="textOnly"
-            severity="secondary"
-            iconLeft="pi pi-user"
-          />
-          <Button variant="filled" iconRight="pi pi-user" />
-          <Button
-            label="Secondary"
-            variant="filled"
-            severity="secondary"
-            iconLeft="pi pi-check"
-          />
-          <Button
-            label="Alternate"
-            variant="filled"
-            severity="alternate"
-            iconRight="pi pi-times"
-          />
-          <Button
-            label="Alternate"
-            size="medium"
-            severity="alternate"
-            variant="outlined"
-            loading={true}
-            iconLeft="pi pi-spin pi-spinner"
-          />
-        </div>
-      </Section>
+        <h3>{t("button.usage.icons")}</h3>
+        <Icon />
+        <CodeBlock exampleCode={Icon({ isString: true }) as string}></CodeBlock>
 
-      <Section title={t("button.usage.disabled")}>
-        <div className="demo-buttons-container">
-          <Button
-            variant="textOnly"
-            severity="primary"
-            iconLeft="pi pi-user"
-            disabled={true}
-          />
-          <Button
-            label="Primary"
-            variant="filled"
-            severity="primary"
-            disabled={true}
-          />
-          <Button
-            label="Outlined"
-            variant="outlined"
-            severity="primary"
-            disabled={true}
-          />
-        </div>
-      </Section>
-
-      <Section title={t("button.usage.link")}>
-        <div className="demo-buttons-container">
-          <Button label="Home" to="/" />
-          <Button label="Back" severity="secondary" to="/ui" />
-        </div>
-      </Section>
-
-      <Section title={t("button.usage.tooltip")}>
-        <div className="demo-buttons-container">
-          <Button
-            variant="textOnly"
-            severity="primary"
-            iconLeft="pi pi-user"
-            title={t("button.message")}
-          />
-        </div>
+        <h3>{t("button.usage.link")}</h3>
+        <Link />
+        <CodeBlock exampleCode={Link({ isString: true }) as string}></CodeBlock>
       </Section>
     </Page>
   );
---
+++ apps/demo/src/Views/Ui/components/Button/components.tsx
@@ -0,0 +1,165 @@
+import { Button } from "@dzangolab/react-ui";
+
+const Severity = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => {
+  if (isString) {
+    return `<div className="demo-buttons-container">
+      <Button label="Primary" severity="primary" />
+      <Button label="Secondary" severity="secondary" />
+      <Button label="Alternate" severity="alternate" />
+      <Button label="Success" severity="success" />
+      <Button label="Danger" severity="danger" />
+      <Button label="Warning" severity="warning" />
+</div>`;
+  }
+
+  return (
+    <div className="demo-buttons-container">
+      <Button label="Primary" severity="primary" />
+      <Button label="Secondary" severity="secondary" />
+      <Button label="Alternate" severity="alternate" />
+      <Button label="Success" severity="success" />
+      <Button label="Danger" severity="danger" />
+      <Button label="Warning" severity="warning" />
+    </div>
+  );
+};
+
+const Size = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => {
+  if (isString) {
+    return `<div className="demo-buttons-container">
+      <Button label="Small" size="small" variant="filled" severity="primary" />
+      <Button
+        label="Medium"
+        size="medium"
+        variant="filled"
+        severity="primary"
+      />
+      <Button label="Large" size="large" variant="filled" severity="primary" />
+</div>`;
+  }
+
+  return (
+    <div className="demo-buttons-container">
+      <Button label="Small" size="small" variant="filled" severity="primary" />
+      <Button
+        label="Medium"
+        size="medium"
+        variant="filled"
+        severity="primary"
+      />
+      <Button label="Large" size="large" variant="filled" severity="primary" />
+    </div>
+  );
+};
+
+const Variant = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => {
+  if (isString) {
+    return `<div className="demo-buttons-container">
+      <Button label="Default" size="medium" />
+      <Button label="Outlined" size="medium" variant="outlined" />
+      <Button label="Text only" size="medium" variant="textOnly" />
+</div>`;
+  }
+
+  return (
+    <div className="demo-buttons-container">
+      <Button label="Default" size="medium" />
+      <Button label="Outlined" size="medium" variant="outlined" />
+      <Button label="Text only" size="medium" variant="textOnly" />
+    </div>
+  );
+};
+
+const Icon = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => {
+  if (isString) {
+    return `<div className="demo-buttons-container">
+      <Button variant="textOnly" severity="secondary" iconLeft="pi pi-user" />
+      <Button variant="filled" iconRight="pi pi-user" />
+      <Button
+        label="Secondary"
+        variant="filled"
+        severity="secondary"
+        iconLeft="pi pi-check"
+      />
+      <Button
+        label="Alternate"
+        variant="filled"
+        severity="alternate"
+        iconRight="pi pi-times"
+      />
+      <Button
+        label="Alternate"
+        size="medium"
+        severity="alternate"
+        variant="outlined"
+        loading={true}
+        iconLeft="pi pi-spin pi-spinner"
+      />
+</div>`;
+  }
+
+  return (
+    <div className="demo-buttons-container">
+      <Button variant="textOnly" severity="secondary" iconLeft="pi pi-user" />
+      <Button variant="filled" iconRight="pi pi-user" />
+      <Button
+        label="Secondary"
+        variant="filled"
+        severity="secondary"
+        iconLeft="pi pi-check"
+      />
+      <Button
+        label="Alternate"
+        variant="filled"
+        severity="alternate"
+        iconRight="pi pi-times"
+      />
+      <Button
+        label="Alternate"
+        size="medium"
+        severity="alternate"
+        variant="outlined"
+        loading={true}
+        iconLeft="pi pi-spin pi-spinner"
+      />
+    </div>
+  );
+};
+
+const Link = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => {
+  if (isString) {
+    return `<div className="demo-buttons-container">
+      <Button label="Home" to="/" />
+      <Button label="Back" severity="secondary" to="/ui" />
+</div>`;
+  }
+
+  return (
+    <div className="demo-buttons-container">
+      <Button label="Home" to="/" />
+      <Button label="Back" severity="secondary" to="/ui" />
+    </div>
+  );
+};
+
+export { Severity, Size, Variant, Icon, Link };
