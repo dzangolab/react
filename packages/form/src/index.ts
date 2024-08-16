696d6a378f836d4c24eb9ed21b9d0e122b660046
shailesh <shailesh@dzangolab.com>
2024-08-16 11:44:12 +0545

    feat(form): export controller and its types

--- packages/form/src/index.ts
+++ packages/form/src/index.ts
@@ -1,3 +1,11 @@
+import {
+  Control,
+  Controller,
+  ControllerFieldState,
+  ControllerProps,
+  ControllerRenderProps,
+} from "react-hook-form";
+
 import "./assets/css/index.css";
 
 export * from "./components";
@@ -7,3 +15,12 @@ export * from "./hooks";
 export * from "./schemas";
 
 export * from "./types";
+
+export { Controller };
+
+export type {
+  Control,
+  ControllerFieldState,
+  ControllerProps,
+  ControllerRenderProps,
+};
