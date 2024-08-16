4758ba638ee33ae3012b4ee6e1c8ba1d3d78d407
shailesh <shailesh@dzangolab.com>
2024-08-16 13:35:42 +0545

    refactor: page size logic

--- packages/ui/src/Table/Table.tsx
+++ packages/ui/src/Table/Table.tsx
@@ -93,7 +93,9 @@ const DataTable = <TData extends { id: string | number }>({
   const [rowSelection, setRowSelection] = useState({});
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: DEFAULT_PAGE_INDEX,
-    pageSize: rowPerPage || (paginated ? DEFAULT_PAGE_SIZE : totalRecords),
+    pageSize: paginated
+      ? rowPerPage || DEFAULT_PAGE_SIZE
+      : totalRecords || data.length,
   });
   const [isFilterRowVisible, setIsFilterRowVisible] = useState(false);
 
