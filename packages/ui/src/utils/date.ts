e997ff4a0ce21610b173168c630807fddeeb8181
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-11-22 11:09:16 +0545

    fix: define default date time options

--- packages/ui/src/utils/date.ts
+++ packages/ui/src/utils/date.ts
@@ -1,7 +1,21 @@
+const defaultOptions: Intl.DateTimeFormatOptions = {
+  year: "numeric",
+  month: "short",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true,
+};
+
 export const formatDate = (
   date: number | string,
   locale?: string,
   options?: Intl.DateTimeFormatOptions,
 ) => {
-  return new Date(date).toLocaleDateString(locale, options);
+  const dateFormatOptions = { ...defaultOptions, ...options };
+
+  return new Date(date).toLocaleDateString(
+    locale || "en-GB",
+    dateFormatOptions,
+  );
 };
