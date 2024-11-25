9c9aaad2baa85351aa0f250257cd9e578da015bf
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-11-25 13:13:03 +0545

    fix: use formatDate util

--- packages/ui/src/Table/TableBody.tsx
+++ packages/ui/src/Table/TableBody.tsx
@@ -7,7 +7,8 @@ import {
   TooltipWrapper,
   TableBody as TTableBody,
 } from "./TableElements";
-import { formatDate, formatNumber, getAlignValue } from "./utils";
+import { formatNumber, getAlignValue } from "./utils";
+import { formatDate } from "..";
 
 import type { TDataTableProperties } from "./types";
 
@@ -74,11 +75,11 @@ export const TableBody = <TData extends { id: string | number }>({
                       }) as NoInfer<never>,
                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     date: (value: any) =>
-                      formatDate({
-                        date: value as Date,
-                        locale: dateOptions?.locale,
-                        formatOptions: dateOptions?.formatOptions,
-                      }) as NoInfer<never>,
+                      formatDate(
+                        value,
+                        dateOptions?.locale,
+                        dateOptions?.formatOptions,
+                      ) as NoInfer<never>,
                     // eslint-disable-next-line @typescript-eslint/no-explicit-any
                     currency: (value: any) =>
                       formatNumber({
