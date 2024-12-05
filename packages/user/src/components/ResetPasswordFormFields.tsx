b3abf60f1541eba6e5e3ce2da54f0155b0845f91
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-12-05 15:22:59 +0545

    fix: update reset password form

---
+++ packages/user/src/components/ResetPasswordFormFields.tsx
@@ -0,0 +1,46 @@
+import { FormActions, Password, useFormContext } from "@dzangolab/react-form";
+import { useTranslation } from "@dzangolab/react-i18n";
+import React from "react";
+
+interface Properties {
+  loading?: boolean;
+}
+
+const ResetPasswordFormFields = ({ loading }: Properties) => {
+  const { t } = useTranslation("user");
+
+  const {
+    register,
+    getFieldState,
+    formState: { errors, submitCount, isDirty },
+  } = useFormContext();
+
+  return (
+    <>
+      <Password
+        label={t("resetPassword.form.newPassword.label")}
+        name="password"
+        register={register}
+        getFieldState={getFieldState}
+      />
+      <Password
+        label={t("resetPassword.form.confirmPassword.label")}
+        name="confirmPassword"
+        register={register}
+        getFieldState={getFieldState}
+      />
+      <FormActions
+        actions={[
+          {
+            id: "submit",
+            label: t("resetPassword.form.actions.submit"),
+          },
+        ]}
+        loading={loading}
+        alignment="fill"
+      />
+    </>
+  );
+};
+
+export default ResetPasswordFormFields;
