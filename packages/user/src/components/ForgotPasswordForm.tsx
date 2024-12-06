eded7b36818bc51bf49392a42f1a9e40b549a765
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2024-12-06 13:17:37 +0545

    fix: disable button

--- packages/user/src/components/ForgotPasswordForm.tsx
+++ packages/user/src/components/ForgotPasswordForm.tsx
@@ -1,13 +1,9 @@
-import {
-  Email,
-  Provider,
-  FormActions,
-  emailSchema,
-} from "@dzangolab/react-form";
+import { Provider, emailSchema } from "@dzangolab/react-form";
 import { useTranslation } from "@dzangolab/react-i18n";
 import React from "react";
 import * as zod from "zod";
 
+import ForgotPasswordFormFields from "./ForgotPasswordFormFields";
 interface Properties {
   handleSubmit: (email: string) => void;
   loading?: boolean;
@@ -28,22 +24,7 @@ export const ForgotPasswordForm = ({ handleSubmit, loading }: Properties) => {
       validationSchema={ForgotPasswordFormSchema}
       onSubmit={(data) => handleSubmit(data.email)}
     >
-      <Email
-        label={t("forgotPassword.form.email.label")}
-        name="email"
-        placeholder={t("forgotPassword.form.email.placeholder")}
-      />
-
-      <FormActions
-        actions={[
-          {
-            id: "submit",
-            label: t("forgotPassword.form.actions.submit"),
-          },
-        ]}
-        loading={loading}
-        alignment="fill"
-      />
+      <ForgotPasswordFormFields loading={loading} />
     </Provider>
   );
 };
