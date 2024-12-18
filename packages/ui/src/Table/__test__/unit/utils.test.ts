ccc5700b9a68631fed289757d08b2f3e7cc9bf68
Bishal Shrestha <143797318+bishalshrestha013@users.noreply.github.com>
2024-12-18 11:53:12 +0545

    refactor: remove unused utils from the table module (#1295)

--- packages/ui/src/Table/__test__/unit/utils.test.ts
+++ packages/ui/src/Table/__test__/unit/utils.test.ts
@@ -1,62 +1,6 @@
 import { describe, expect, test } from "vitest";
 
-import { formatDate, formatNumber } from "../../utils";
-
-describe("FormatDate unit test", () => {
-  test("Should return a US locale based formatted date", () => {
-    const date = new Date("1/9/2024");
-    const locale = "en-US";
-
-    expect(formatDate({ date, locale })).toBe("1/9/2024");
-  });
-
-  test("Should return a UK locale based formatted date", () => {
-    const date = new Date("1/9/2024");
-    const locale = "en-GB";
-
-    expect(formatDate({ date, locale })).toBe("09/01/2024");
-  });
-
-  test("Should return a default formatted date", () => {
-    const date = new Date("1/9/2024");
-    const locale = "en-US";
-
-    expect(formatDate({ date, locale })).toBe("1/9/2024");
-  });
-
-  test("Should return a Nepal locale based formatted date", () => {
-    const date = new Date("1/9/2024");
-    const locale = "ne-NP";
-
-    expect(formatDate({ date, locale })).toBe("२०२४-०१-०९");
-  });
-
-  test("Should return passed date value if not a valid date", () => {
-    const date = "date";
-    const locale = "ne-NP";
-
-    expect(formatDate({ date, locale })).toBe("date");
-  });
-
-  test("Should throw error if invalid locale is passed", () => {
-    const date = new Date("1/9/2024");
-    const locale = "ne-N";
-
-    try {
-      formatDate({ date, locale });
-    } catch (error) {
-      expect(() => {
-        throw error;
-      }).toThrowError("Invalid language tag: ne-n");
-    }
-  });
-
-  test("Should return null if date is empty", () => {
-    const date = "";
-
-    expect(formatDate({ date })).toBeNull();
-  });
-});
+import { formatNumber } from "../../utils";
 
 describe("FormatNumber unit test", () => {
   test("Should return a US locale based formatted number", () => {
