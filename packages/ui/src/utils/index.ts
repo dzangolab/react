a2a89bf67cf90a5055e5138276e0041a25e7ed1d
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2024-12-31 15:16:33 +0545

    feat(ui): remove useManipulatedColumns hook (#1314)

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -3,6 +3,5 @@ export * from "./UseExporter";
 export * from "./UseExporterAsync";
 export * from "./UseMediaQuery";
 export * from "./UseColumnsMap";
-export * from "./UseManipulateColumns";
 export * from "./UseDebouncedValue";
 export * from "./storage";
