096914aafa0748dfff9e92f00c376d3c7868d7b4
Bishal Shrestha <bishal.shrestha@dzangolab.com>
2025-01-09 13:23:03 +0545

    feat: update tabview demo

--- apps/demo/src/Views/Ui/components/TabbedPanel/TabbedPanel.tsx
+++ apps/demo/src/Views/Ui/components/TabbedPanel/TabbedPanel.tsx
@@ -1,7 +1,14 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { TabbedPanel, Page, SubPane } from "@dzangolab/react-ui";
 
-import { Basic } from "./components";
+import {
+  Basic,
+  CustomActive,
+  DisableStatePersistence,
+  PositionBottom,
+  PositionLeft,
+  PositionRight,
+} from "./components";
 import { CodeBlock } from "../../../../components/Demo";
 import { Section } from "../../../../components/Demo";
 
@@ -17,39 +24,34 @@ export const TabbedPanelDemo = () => {
         ></CodeBlock>
       </Section>
       <Section title={t("tabbedPanel.usage.customActiveTab")}>
-        <TabbedPanel id="tabbed-pannel-2" defaultActiveIndex={1}>
-          <SubPane title="Tab 1">Tab 1 content</SubPane>
-          <SubPane title="Tab 2">Tab 2 content</SubPane>
-          <SubPane title="Tab 3">Tab 3 content</SubPane>
-        </TabbedPanel>
+        <CustomActive />
+        <CodeBlock
+          exampleCode={CustomActive({ isString: true }) as string}
+        ></CodeBlock>
       </Section>
       <Section title={t("tabbedPanel.usage.disableTabState")}>
-        <TabbedPanel id="tabbed-pannel-3" persistState={false}>
-          <SubPane title="Tab 1">Tab 1 content</SubPane>
-          <SubPane title="Tab 2">Tab 2 content</SubPane>
-          <SubPane title="Tab 3">Tab 3 content</SubPane>
-        </TabbedPanel>
+        <DisableStatePersistence />
+        <CodeBlock
+          exampleCode={DisableStatePersistence({ isString: true }) as string}
+        ></CodeBlock>
       </Section>
       <Section title={t("tabbedPanel.usage.positionBottom")}>
-        <TabbedPanel id="tabbed-pannel-4" position="bottom">
-          <SubPane title="Tab 1">Tab 1 content</SubPane>
-          <SubPane title="Tab 2">Tab 2 content</SubPane>
-          <SubPane title="Tab 3">Tab 3 content</SubPane>
-        </TabbedPanel>
+        <PositionBottom />
+        <CodeBlock
+          exampleCode={PositionBottom({ isString: true }) as string}
+        ></CodeBlock>
       </Section>
       <Section title={t("tabbedPanel.usage.positionLeft")}>
-        <TabbedPanel id="tabbed-pannel-5" position="left">
-          <SubPane title="Tab 1">Tab 1 content</SubPane>
-          <SubPane title="Tab 2">Tab 2 content</SubPane>
-          <SubPane title="Tab 3">Tab 3 content</SubPane>
-        </TabbedPanel>
+        <PositionLeft />
+        <CodeBlock
+          exampleCode={PositionLeft({ isString: true }) as string}
+        ></CodeBlock>
       </Section>
       <Section title={t("tabbedPanel.usage.positionRight")}>
-        <TabbedPanel id="tabbed-pannel-6" position="right">
-          <SubPane title="Tab 1">Tab 1 content</SubPane>
-          <SubPane title="Tab 2">Tab 2 content</SubPane>
-          <SubPane title="Tab 3">Tab 3 content</SubPane>
-        </TabbedPanel>
+        <PositionRight />
+        <CodeBlock
+          exampleCode={PositionRight({ isString: true }) as string}
+        ></CodeBlock>
       </Section>
     </Page>
   );
--- apps/demo/src/Views/Ui/components/TabbedPanel/components.tsx
+++ apps/demo/src/Views/Ui/components/TabbedPanel/components.tsx
@@ -1,12 +1,47 @@
-import { Button, SubPane, TabbedPanel } from "@dzangolab/react-ui";
+import { SubPane, TabbedPanel } from "@dzangolab/react-ui";
 
-const Basic = ({
+const TabbedPanelComponent = ({
+  id,
+  defaultActiveIndex,
+  persistState,
+  position,
   isString = false,
 }: {
+  id: string;
+  defaultActiveIndex?: number;
+  persistState?: boolean;
+  position?: "top" | "bottom" | "left" | "right";
   isString?: boolean;
 }): JSX.Element | string => {
+  const tabbedPanelProperties = {
+    id,
+    defaultActiveIndex,
+    persistState,
+    position,
+  };
+
+  const content = (
+    <TabbedPanel {...tabbedPanelProperties}>
+      <SubPane title="Description">Description</SubPane>
+      <SubPane title="Reviews">Reviews</SubPane>
+      <SubPane title="Specifications">Specifications</SubPane>
+      <SubPane title="Pricing">Pricing</SubPane>
+    </TabbedPanel>
+  );
+
   if (isString) {
-    return `<TabbedPanel id="tabbed-pannel-1">
+    const propertiesString = [
+      `id="${id}"`,
+      defaultActiveIndex !== undefined
+        ? `defaultActiveIndex={${defaultActiveIndex}}`
+        : "",
+      persistState === false ? "persistState={false}" : "",
+      position ? `position="${position}"` : "",
+    ]
+      .filter(Boolean)
+      .join(" ");
+
+    return `<TabbedPanel ${propertiesString}>
   <SubPane title="Description">Description</SubPane>
   <SubPane title="Reviews">Reviews</SubPane>
   <SubPane title="Specifications">Specifications</SubPane>
@@ -14,14 +49,82 @@ const Basic = ({
 </TabbedPanel>`;
   }
 
-  return (
-    <TabbedPanel id="tabbed-pannel-1">
-      <SubPane title="Description">Description</SubPane>
-      <SubPane title="Reviews">Reviews</SubPane>
-      <SubPane title="Specifications">Specifications</SubPane>
-      <SubPane title="Pricing">Pricing</SubPane>
-    </TabbedPanel>
-  );
+  return content;
 };
 
-export { Basic };
+const Basic = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent id="tabbed-pannel-1" isString={isString} />
+);
+
+const CustomActive = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent
+    id="tabbed-pannel-2"
+    defaultActiveIndex={1}
+    isString={isString}
+  />
+);
+
+const DisableStatePersistence = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent
+    id="tabbed-pannel-3"
+    persistState={false}
+    isString={isString}
+  />
+);
+
+const PositionBottom = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent
+    id="tabbed-pannel-4"
+    position="bottom"
+    isString={isString}
+  />
+);
+
+const PositionLeft = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent
+    id="tabbed-pannel-5"
+    position="left"
+    isString={isString}
+  />
+);
+
+const PositionRight = ({
+  isString = false,
+}: {
+  isString?: boolean;
+}): JSX.Element | string => (
+  <TabbedPanelComponent
+    id="tabbed-pannel-6"
+    position="right"
+    isString={isString}
+  />
+);
+
+export {
+  Basic,
+  CustomActive,
+  DisableStatePersistence,
+  PositionBottom,
+  PositionLeft,
+  PositionRight,
+};
