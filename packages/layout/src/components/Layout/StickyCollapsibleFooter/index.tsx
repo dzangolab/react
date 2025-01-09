b90b11529e5751bee2a2582dc2e1adad451a2122
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-01-09 12:44:47 +0545

    fix: make hover area and component area equal

--- packages/layout/src/components/Layout/StickyCollapsibleFooter/index.tsx
+++ packages/layout/src/components/Layout/StickyCollapsibleFooter/index.tsx
@@ -8,7 +8,6 @@ export const StickyCollapsibleFooter = ({ children }: Properties) => {
   return (
     <>
       <div className="sticky-collapsible-footer-container">
-        <div className="sticky-collapsible-footer-placeholder"></div>
         <div className="footer">{children}</div>
       </div>
     </>
