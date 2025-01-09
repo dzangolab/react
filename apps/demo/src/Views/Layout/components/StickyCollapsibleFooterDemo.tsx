a2d51010faf86f0365af6af9ae162d98297b75e4
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-01-09 16:02:19 +0545

    fix: use position absolute instead of fixed

--- apps/demo/src/Views/Layout/components/StickyCollapsibleFooterDemo.tsx
+++ apps/demo/src/Views/Layout/components/StickyCollapsibleFooterDemo.tsx
@@ -1,6 +1,6 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { StickyCollapsibleFooter } from "@dzangolab/react-layout";
-import { Page } from "@dzangolab/react-ui";
+import { Card, CardBody, Page } from "@dzangolab/react-ui";
 
 export const StickyCollapsibleFooterDemo = () => {
   const [t] = useTranslation("layout");
@@ -9,10 +9,16 @@ export const StickyCollapsibleFooterDemo = () => {
     <Page
       title={t("stickyCollapsibleFooter.title")}
       subtitle={t("stickyCollapsibleFooter.subtitle")}
+      className="sticky-collapsible-footer"
     >
-      <StickyCollapsibleFooter>
-        This is sticky collapsible footer component.
-      </StickyCollapsibleFooter>
+      <Card outlined>
+        <CardBody>
+          <div style={{ height: "20vh" }}>Body content</div>
+        </CardBody>
+        <StickyCollapsibleFooter>
+          This is sticky collapsible footer component.
+        </StickyCollapsibleFooter>
+      </Card>
     </Page>
   );
 };
