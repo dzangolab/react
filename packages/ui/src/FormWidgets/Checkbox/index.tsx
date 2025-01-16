485b3b7a87f78b4cbc0f95eab6ad59e68d201f47
Bishal Shrestha <143797318+bishalshrestha013@users.noreply.github.com>
2025-01-16 12:27:50 +0545

    refactor(ui): update checkbox component (#1342)
    * refactor: update checkbox component
    
    * feat: change the prop name modelValue to checked

--- packages/ui/src/FormWidgets/Checkbox/index.tsx
+++ packages/ui/src/FormWidgets/Checkbox/index.tsx
@@ -1,27 +1,36 @@
 import { InputHTMLAttributes } from "react";
 
 interface IProperties extends InputHTMLAttributes<HTMLInputElement> {
-  label?: string;
   errorMessage?: string;
+  helperText?: string;
+  label?: string | React.ReactNode;
 }
 
 export const Checkbox: React.FC<IProperties> = ({
-  name,
-  label,
-  onChange,
+  checked,
+  disabled,
   errorMessage,
+  helperText,
+  label,
+  name,
+  onChange,
+  value,
   ...others
 }) => {
   return (
     <div className="checkbox-wrapper">
       <input
-        {...others}
+        checked={checked}
+        disabled={disabled}
         id={name}
-        type="checkbox"
         name={name}
         onChange={onChange}
+        type="checkbox"
+        value={value}
+        {...others}
       />
       {label && <label htmlFor={name}>{label}</label>}
+      {helperText && <span className="helper-text">{helperText}</span>}
       {errorMessage && <div className="error-message">{errorMessage}</div>}
     </div>
   );
