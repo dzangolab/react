f5d92ab65e57ee47ddc77f95b021c8704a09f464
lamdiki sherpa <lamdiki.sherpa@dzangolab.com>
2025-01-22 16:07:05 +0545

    feat(demo): add code example for submit button  (#1356)
    * fix: add code example
    
    * fix: update translation message

--- apps/demo/src/Views/Ui/components/SubmitButton.tsx
+++ apps/demo/src/Views/Ui/components/SubmitButton.tsx
@@ -1,18 +1,71 @@
 import { useTranslation } from "@dzangolab/react-i18n";
 import { Page, SubmitButton } from "@dzangolab/react-ui";
+import { TDataTable } from "@dzangolab/react-ui";
 
-import { Section } from "../../../components/Demo";
+import { CodeBlock, Section } from "../../../components/Demo";
+
+const data = [
+  {
+    id: 1,
+    prop: "disabled",
+    type: "boolean",
+    description: "If true, the button will be disabled and non-interactive.",
+  },
+  {
+    id: 2,
+    prop: "label",
+    type: "string",
+    description: "Text to be displayed in the button.",
+  },
+  {
+    id: 3,
+    prop: "loading",
+    type: "boolean",
+    description:
+      "If true, indicates that the button is in a loading state and disables user interaction.",
+  },
+];
 
 export const SubmitButtonDemo = () => {
   const [t] = useTranslation("ui");
 
   return (
     <Page title={t("submitButton.title")}>
-      <Section>
-        <SubmitButton label="Submit" />
+      <Section title={t("headers.usage")}>
+        <p>{t("common.usage", { component: "SubmitButton" })}</p>
+        <CodeBlock exampleCode="import { SubmitButton } from '@dzangolab/react-ui';" />
       </Section>
-      <Section>
+      <Section title={t("submitButton.usage.basic")}>
+        <SubmitButton label="Submit" />
+        <CodeBlock exampleCode="<SubmitButton label='Submit'/>" />
+      </Section>
+      <Section title={t("submitButton.usage.loading")}>
         <SubmitButton label="Submit loading" loading />
+        <CodeBlock exampleCode="<SubmitButton label='Submit loading' loading/>" />
+      </Section>
+      <Section
+        title={t("headers.propertiesValue", {
+          value: "SubmitButtonProperties",
+        })}
+      >
+        <TDataTable
+          columns={[
+            {
+              accessorKey: "prop",
+              header: "Properties",
+            },
+            {
+              accessorKey: "type",
+              header: "Type",
+            },
+            {
+              accessorKey: "description",
+              header: "Description",
+            },
+          ]}
+          data={data}
+          paginated={false}
+        />
       </Section>
     </Page>
   );
