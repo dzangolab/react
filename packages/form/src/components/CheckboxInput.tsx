12328acf22e60cf8f238dd7dc50783c578833c80
shailesh <shailesh@dzangolab.com>
2025-01-29 17:10:38 +0545

    feat(form): update CheckboxInput type

--- packages/form/src/components/CheckboxInput.tsx
+++ packages/form/src/components/CheckboxInput.tsx
@@ -1,24 +1,9 @@
-import { CheckboxInput as BasicCheckboxInput } from "@dzangolab/react-ui";
+import {
+  CheckboxInput as BasicCheckboxInput,
+  ICheckboxInputProperties,
+} from "@dzangolab/react-ui";
 import React from "react";
 import { Controller, useFormContext } from "react-hook-form";
-
-type Option<T> = {
-  value: T;
-  label: string;
-};
-
-export type ICheckboxInputProperties<T> = {
-  className?: string;
-  direction?: "horizontal" | "vertical";
-  disabled?: boolean;
-  checked?: boolean;
-  helperText?: string;
-  inputLabel?: string;
-  label?: string | React.ReactNode;
-  name: string;
-  options: Option<T>[];
-  placeholder?: string;
-};
 
 export const CheckboxInput = <T extends string | number>({
   className,
