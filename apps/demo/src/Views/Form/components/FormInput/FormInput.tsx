bb7b02ace5a934f1db96d18d95666d33dc24f04a
shailesh896 <65159047+shailesh896@users.noreply.github.com>
2025-02-07 13:41:24 +0545

    refactor(ui): update select component to have consistent html (#1374)
    * refactor(ui): update select component to have consistent html
    
    * refactor: add gap between checkbox and label
    
    * refactor: class names
    
    * refactor: style and html
    
    * refactor: rerange css
    
    * refactor: code standard

--- apps/demo/src/Views/Form/components/FormInput/FormInput.tsx
+++ apps/demo/src/Views/Form/components/FormInput/FormInput.tsx
@@ -69,6 +69,7 @@ export const FormInputDemo = () => {
           invalid: false,
           typeahead: "string",
           radioInput: "value 1",
+          select: ["FR"],
         }}
         validationTriggerKey={i18n.language}
       >
