6f2a2a30ad71350bd46df02326e1f6208527929e
shailesh <shailesh@dzangolab.com>
2025-02-07 15:19:35 +0545

    refactor: add word library in page title

--- apps/demo/src/Views/Layout/LayoutPage.tsx
+++ apps/demo/src/Views/Layout/LayoutPage.tsx
@@ -4,5 +4,5 @@ import { Page } from "@dzangolab/react-ui";
 export const LayoutPage = () => {
   const [t] = useTranslation("app");
 
-  return <Page title={t("header.menu.layout")}></Page>;
+  return <Page title={t("pages.layout.title")}></Page>;
 };
