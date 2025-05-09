faaa383458207c6d02b145cd7f15c2ea1f428beb
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-05-09 13:44:40 +0545

    refactor: extend datepicker to support all base props

--- packages/form/src/components/DatePicker.tsx
+++ packages/form/src/components/DatePicker.tsx
@@ -1,29 +1,19 @@
+import {
+  DatePicker as DatePickerBasic,
+  DatePickerProperties,
+} from "@dzangolab/react-ui";
 import { FC } from "react";
 import { Controller, useFormContext } from "react-hook-form";
 
-import { DatePicker as DatePickerBasic } from "@dzangolab/react-ui";
-
-export interface DatePicker {
-  className?: string;
-  dateFormat?: string;
-  label?: string;
-  maxDate?: Date;
-  minDate?: Date;
+interface DatePicker
+  extends Omit<
+    DatePickerProperties,
+    "onChange" | "value" | "error" | "inputRef"
+  > {
   name: string;
-  panelClassName?: string;
-  placeholder?: string;
 }
 
-export const DatePicker: FC<DatePicker> = ({
-  className,
-  dateFormat,
-  label,
-  maxDate,
-  minDate,
-  name,
-  panelClassName,
-  placeholder,
-}) => {
+export const DatePicker: FC<DatePicker> = ({ name, ...others }) => {
   const { control, getFieldState } = useFormContext();
   const { error } = getFieldState(name);
 
@@ -35,17 +25,11 @@ export const DatePicker: FC<DatePicker> = ({
         render={({ field }) => (
           <DatePickerBasic
             inputRef={field.ref}
-            className={className}
             name={name}
-            maxDate={maxDate}
-            minDate={minDate}
             onChange={field.onChange}
-            error={error?.message}
-            label={label}
-            panelClassName={panelClassName}
-            placeholder={placeholder}
             value={field.value}
-            dateFormat={dateFormat}
+            error={error?.message}
+            {...others}
           />
         )}
       />
