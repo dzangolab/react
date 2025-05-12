bdcd97ecb68188fee6903d84834b9d2d6a813a39
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-05-12 15:18:27 +0545

    refactor: split component into multiple render

--- packages/ui/src/Table/TableHeader.tsx
+++ packages/ui/src/Table/TableHeader.tsx
@@ -54,168 +54,184 @@ export const TableHeader = <TData extends RowData>({
 
     return null;
   };
-
-  return (
-    <TTableHeader>
-      {table.getHeaderGroups().map((headerGroup) => (
-        <TableRow key={headerGroup.id} className="header-row">
-          {headerGroup.headers.map(
-            ({ column, getContext, id, isPlaceholder, colSpan }) => {
-              const {
-                columnDef,
-                getCanSort,
-                getIsSorted,
-                getToggleSortingHandler,
-              } = column;
-
-              const activeColumnClass = `${
-                getIsSorted() === "asc" || getIsSorted() === "desc"
-                  ? "highlight"
-                  : ""
-              }`;
-
-              if (!isFilterRowVisible && column.getCanFilter()) {
-                setIsFilterRowVisible(true);
-              }
-
-              const getSortIcon = () => {
-                if (renderSortIcons) {
-                  return renderSortIcons(getIsSorted());
-                }
-
-                switch (getIsSorted()) {
-                  case "asc":
-                    return <i className="pi pi-sort-up-fill"></i>;
-                  case "desc":
-                    return <i className="pi pi-sort-down-fill"></i>;
-                  default:
-                    return <i className="pi pi-sort"></i>;
-                }
-              };
-
-              return (
-                <ColumnHeader
-                  key={id}
-                  colSpan={colSpan}
-                  className={`column-${id} ${
-                    columnDef.className || ""
-                  } ${activeColumnClass} ${
-                    columnDef.enableSorting ? "sortable" : ""
-                  }`
-                    .replace(/\s\s/, " ")
-                    .trimEnd()}
-                  data-align={getAlignValue({
-                    align: columnDef.align,
-                    dataType: columnDef.dataType,
-                  })}
-                  style={{
-                    width: columnDef.width,
-                    maxWidth: columnDef.maxWidth,
-                    minWidth: columnDef.minWidth,
-                  }}
-                  onClick={(event) => {
-                    if (getCanSort()) {
-                      handleSort(event, getToggleSortingHandler());
-                    }
-                  }}
-                >
-                  <>
-                    {isPlaceholder ? null : (
-                      <>
-                        {flexRender(columnDef.header, getContext())}
-
-                        {getCanSort() ? (
-                          <span className="sort-state">{getSortIcon()}</span>
-                        ) : null}
-                      </>
-                    )}
-                  </>
-                </ColumnHeader>
-              );
-            },
-          )}
-        </TableRow>
-      ))}
-
-      {isFilterRowVisible ? (
-        <TableRow key={"filters"} className={`header-row filters`}>
-          {table.getVisibleLeafColumns().map((column) => {
-            if (!column.getCanFilter()) {
-              return <ColumnHeader key={"filter" + column.id}></ColumnHeader>;
-            }
+  const renderHeaderRow = () =>
+    table.getHeaderGroups().map((headerGroup) => (
+      <TableRow key={headerGroup.id} className="header-row">
+        {headerGroup.headers.map(
+          ({ column, getContext, id, isPlaceholder, colSpan }) => {
+            const {
+              columnDef,
+              getCanSort,
+              getIsSorted,
+              getToggleSortingHandler,
+            } = column;
 
             const activeColumnClass = `${
-              column.getIsFiltered() ? "highlight" : ""
+              getIsSorted() === "asc" || getIsSorted() === "desc"
+                ? "highlight"
+                : ""
             }`;
 
-            const filterColumnClass = `${column.getCanFilter() ? `filter ${column.columnDef.meta?.filterVariant}` : ""}`;
+            if (!isFilterRowVisible && column.getCanFilter()) {
+              setIsFilterRowVisible(true);
+            }
 
-            const columnFilterValue = column.getFilterValue();
+            const getSortIcon = () => {
+              if (renderSortIcons) {
+                return renderSortIcons(getIsSorted());
+              }
+
+              switch (getIsSorted()) {
+                case "asc":
+                  return <i className="pi pi-sort-up-fill"></i>;
+                case "desc":
+                  return <i className="pi pi-sort-down-fill"></i>;
+                default:
+                  return <i className="pi pi-sort"></i>;
+              }
+            };
 
             return (
               <ColumnHeader
-                key={"filter" + column.id}
-                data-label={column.id}
-                data-align={getAlignValue({
-                  align: column.columnDef.align,
-                  dataType: column.columnDef.dataType,
-                })}
-                style={{
-                  width: column.columnDef.width,
-                  maxWidth: column.columnDef.maxWidth,
-                  minWidth: column.columnDef.minWidth,
-                }}
-                className={`${
-                  column.id ? `column-${column.id}` : ``
-                } ${activeColumnClass} ${column.columnDef.className || ""} ${filterColumnClass}`
+                key={id}
+                colSpan={colSpan}
+                className={`column-${id} ${
+                  columnDef.className || ""
+                } ${activeColumnClass} ${
+                  columnDef.enableSorting ? "sortable" : ""
+                }`
                   .replace(/\s\s/, " ")
                   .trimEnd()}
+                data-align={getAlignValue({
+                  align: columnDef.align,
+                  dataType: columnDef.dataType,
+                })}
+                style={{
+                  width: columnDef.width,
+                  maxWidth: columnDef.maxWidth,
+                  minWidth: columnDef.minWidth,
+                }}
+                onClick={(event) => {
+                  if (getCanSort()) {
+                    handleSort(event, getToggleSortingHandler());
+                  }
+                }}
               >
-                {column.columnDef.customFilterComponent ? (
-                  column.columnDef.customFilterComponent(column)
-                ) : column.columnDef.meta?.filterVariant === "multiselect" ? (
-                  <Select
-                    name="multiselect"
-                    placeholder={column.columnDef.filterPlaceholder || ""}
-                    options={column.columnDef.meta?.filterOptions || []}
-                    value={(columnFilterValue as string[]) || []}
-                    onChange={(value) => {
-                      column.setFilterValue(value);
-                    }}
-                    multiple={true}
-                  />
-                ) : column.columnDef.meta?.filterVariant === "dateRange" ? (
-                  <DatePicker
-                    inputRef={null}
-                    name="date-range"
-                    onChange={(date) => {
-                      if (date) {
-                        const _date = (date as Date[])
-                          .map(getFormattedDate)
-                          .filter((d) => d !== null);
-                        column.setFilterValue(_date);
-                      }
-                    }}
-                    value={convertFilterValueToDate(
-                      column.getFilterValue() as string[],
-                    )}
-                    selectionMode="range"
-                  />
-                ) : (
-                  <DebouncedInput
-                    defaultValue={columnFilterValue as string}
-                    onInputChange={(value) => {
-                      column.setFilterValue(value);
-                    }}
-                    placeholder={column.columnDef.filterPlaceholder || ""}
-                    debounceTime={inputDebounceTime}
-                  ></DebouncedInput>
-                )}
+                <>
+                  {isPlaceholder ? null : (
+                    <>
+                      {flexRender(columnDef.header, getContext())}
+
+                      {getCanSort() ? (
+                        <span className="sort-state">{getSortIcon()}</span>
+                      ) : null}
+                    </>
+                  )}
+                </>
               </ColumnHeader>
             );
-          })}
-        </TableRow>
-      ) : null}
+          },
+        )}
+      </TableRow>
+    ));
+
+  //eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const renderColumnFilter = (column: any) => {
+    if (column.columnDef.customFilterComponent) {
+      return column.columnDef.customFilterComponent(column);
+    }
+
+    const columnFilterValue = column.getFilterValue();
+    const variant = column.columnDef.meta?.filterVariant;
+
+    if (variant === "multiselect") {
+      return (
+        <Select
+          name="multiselect"
+          placeholder={column.columnDef.filterPlaceholder || ""}
+          options={column.columnDef.meta?.filterOptions || []}
+          value={(columnFilterValue as string[]) || []}
+          onChange={(value) => column.setFilterValue(value)}
+          multiple
+        />
+      );
+    }
+
+    if (variant === "dateRange") {
+      return (
+        <DatePicker
+          inputRef={null}
+          name="date-range"
+          onChange={(date) => {
+            if (date) {
+              const _date = (date as Date[])
+                .map(getFormattedDate)
+                .filter((d) => d !== null);
+              column.setFilterValue(_date);
+            }
+          }}
+          value={convertFilterValueToDate(column.getFilterValue() as string[])}
+          selectionMode="range"
+        />
+      );
+    }
+
+    return (
+      <DebouncedInput
+        defaultValue={columnFilterValue as string}
+        onInputChange={(value) => column.setFilterValue(value)}
+        placeholder={column.columnDef.filterPlaceholder || ""}
+        debounceTime={inputDebounceTime}
+      />
+    );
+  };
+
+  const renderFilterRow = () => {
+    if (!isFilterRowVisible) return null;
+
+    return (
+      <TableRow key="filters" className="header-row filters">
+        {table.getVisibleLeafColumns().map((column) => {
+          if (!column.getCanFilter()) {
+            return <ColumnHeader key={"filter" + column.id}></ColumnHeader>;
+          }
+          const activeColumnClass = `${
+            column.getIsFiltered() ? "highlight" : ""
+          }`;
+
+          const filterColumnClass = `${column.getCanFilter() ? `filter ${column.columnDef.meta?.filterVariant}` : ""}`;
+
+          return (
+            <ColumnHeader
+              key={"filter" + column.id}
+              data-label={column.id}
+              data-align={getAlignValue({
+                align: column.columnDef.align,
+                dataType: column.columnDef.dataType,
+              })}
+              style={{
+                width: column.columnDef.width,
+                maxWidth: column.columnDef.maxWidth,
+                minWidth: column.columnDef.minWidth,
+              }}
+              className={`${
+                column.id ? `column-${column.id}` : ``
+              } ${activeColumnClass} ${column.columnDef.className || ""} ${filterColumnClass}`
+                .replace(/\s\s/, " ")
+                .trimEnd()}
+            >
+              {renderColumnFilter(column)}
+            </ColumnHeader>
+          );
+        })}
+      </TableRow>
+    );
+  };
+
+  return (
+    <TTableHeader>
+      {renderHeaderRow()}
+      {renderFilterRow()}
     </TTableHeader>
   );
 };
