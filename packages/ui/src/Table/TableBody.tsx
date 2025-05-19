51e1cd95ae4fb294e814d83965b9d95910a8ee33
lamdiki.sherpa <lamdiki.sherpa@dzangolab.com>
2025-05-19 13:24:14 +0545

    fix: display empty table message after loading state

--- packages/ui/src/Table/TableBody.tsx
+++ packages/ui/src/Table/TableBody.tsx
@@ -22,7 +22,10 @@ import type { TDataTableProperties } from "./types";
 interface TableBodyProperties<TData extends RowData>
   extends Pick<
     TDataTableProperties<TData>,
-    "enableRowSelection" | "customFormatters" | "emptyTableMessage"
+    | "enableRowSelection"
+    | "customFormatters"
+    | "emptyTableMessage"
+    | "isLoading"
   > {
   parsedColumnsLength: number;
   rowClassName?: string | ((options: { row: Row<TData> }) => string);
@@ -33,6 +36,7 @@ export const TableBody = <TData extends RowData>({
   customFormatters = {},
   emptyTableMessage = "No results.",
   enableRowSelection,
+  isLoading,
   parsedColumnsLength,
   rowClassName,
   table,
@@ -63,128 +67,134 @@ export const TableBody = <TData extends RowData>({
 
   return (
     <TTableBody>
-      {table.getRowModel().rows?.length ? (
-        table.getRowModel().rows.map((row) => (
-          <TableRow
-            className={getRowClassName(row)}
-            data-id={(row.original as any).id ?? row.id} // eslint-disable-line @typescript-eslint/no-explicit-any
-            key={row.id}
-            {...(enableRowSelection && {
-              "data-selected": row.getIsSelected(),
-            })}
-          >
-            {row.getVisibleCells().map((cell) => {
-              const getFormattedValueContext: typeof cell.getContext = () => {
-                const cellContext = cell.getContext();
-                const renderValue = cellContext.getValue;
-                const dateOptions = cell.column.columnDef.dateOptions;
-                const numberOptions = cell.column.columnDef.numberOptions;
+      {isLoading ? null : (
+        <>
+          {table.getRowModel().rows.length > 0 ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                className={getRowClassName(row)}
+                data-id={(row.original as any).id ?? row.id} // eslint-disable-line @typescript-eslint/no-explicit-any
+                key={row.id}
+                {...(enableRowSelection && {
+                  "data-selected": row.getIsSelected(),
+                })}
+              >
+                {row.getVisibleCells().map((cell) => {
+                  const getFormattedValueContext: typeof cell.getContext =
+                    () => {
+                      const cellContext = cell.getContext();
+                      const renderValue = cellContext.getValue;
+                      const dateOptions = cell.column.columnDef.dateOptions;
+                      const numberOptions = cell.column.columnDef.numberOptions;
 
-                const getFormattedValue = (): NoInfer<never> => {
-                  const defaultCustomFormatters: Record<
-                    string,
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    (value: any) => NoInfer<never>
-                  > = {
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    number: (value: any) =>
-                      formatNumber({
-                        value: Number(value),
-                        locale: numberOptions?.locale,
-                        formatOptions: numberOptions?.formatOptions,
-                      }) as NoInfer<never>,
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    date: (value: any) =>
-                      formatDate(
-                        value,
-                        dateOptions?.locale,
-                        dateOptions?.formatOptions,
-                      ) as NoInfer<never>,
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    datetime: (value: any) =>
-                      formatDateTime(
-                        value,
-                        dateOptions?.locale,
-                        dateOptions?.formatOptions,
-                      ) as NoInfer<never>,
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    currency: (value: any) =>
-                      formatNumber({
-                        value: Number(value),
-                        locale: numberOptions?.locale,
-                        formatOptions: {
-                          style: "currency",
-                          currency: "USD",
-                          ...(numberOptions?.formatOptions &&
-                            numberOptions.formatOptions),
-                        },
-                      }) as NoInfer<never>,
-                    ...customFormatters,
-                  };
+                      const getFormattedValue = (): NoInfer<never> => {
+                        const defaultCustomFormatters: Record<
+                          string,
+                          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          (value: any) => NoInfer<never>
+                        > = {
+                          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          number: (value: any) =>
+                            formatNumber({
+                              value: Number(value),
+                              locale: numberOptions?.locale,
+                              formatOptions: numberOptions?.formatOptions,
+                            }) as NoInfer<never>,
+                          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          date: (value: any) =>
+                            formatDate(
+                              value,
+                              dateOptions?.locale,
+                              dateOptions?.formatOptions,
+                            ) as NoInfer<never>,
+                          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          datetime: (value: any) =>
+                            formatDateTime(
+                              value,
+                              dateOptions?.locale,
+                              dateOptions?.formatOptions,
+                            ) as NoInfer<never>,
+                          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                          currency: (value: any) =>
+                            formatNumber({
+                              value: Number(value),
+                              locale: numberOptions?.locale,
+                              formatOptions: {
+                                style: "currency",
+                                currency: "USD",
+                                ...(numberOptions?.formatOptions &&
+                                  numberOptions.formatOptions),
+                              },
+                            }) as NoInfer<never>,
+                          ...customFormatters,
+                        };
 
-                  const dataType: string =
-                    cell.column.columnDef.dataType || "text";
+                        const dataType: string =
+                          cell.column.columnDef.dataType || "text";
+
+                        return (
+                          defaultCustomFormatters?.[dataType]?.(
+                            renderValue(),
+                          ) || renderValue()
+                        );
+                      };
+
+                      return {
+                        ...cellContext,
+                        renderValue: () => getFormattedValue(),
+                        getValue: () => getFormattedValue(),
+                      };
+                    };
 
                   return (
-                    defaultCustomFormatters?.[dataType]?.(renderValue()) ||
-                    renderValue()
-                  );
-                };
-
-                return {
-                  ...cellContext,
-                  renderValue: () => getFormattedValue(),
-                  getValue: () => getFormattedValue(),
-                };
-              };
-
-              return (
-                <TableCell
-                  key={cell.id}
-                  data-label={cell.column.id}
-                  data-align={getAlignValue({
-                    align: cell.column.columnDef.align,
-                    dataType: cell.column.columnDef.dataType,
-                  })}
-                  className={`${
-                    cell.column.id ? `cell-${cell.column.id}` : ``
-                  } ${cell.column.columnDef.className || ""}`
-                    .replace(/\s\s/, " ")
-                    .trimEnd()}
-                  style={{
-                    width: cell.column.columnDef.width,
-                    maxWidth: cell.column.columnDef.maxWidth,
-                    minWidth: cell.column.columnDef.minWidth,
-                  }}
-                >
-                  {cell.column.columnDef.tooltip ? (
-                    <TooltipWrapper
-                      tooltipOptions={{
-                        children: renderTooltipContent(cell),
-                        ...cell.column.columnDef?.tooltipOptions,
+                    <TableCell
+                      key={cell.id}
+                      data-label={cell.column.id}
+                      data-align={getAlignValue({
+                        align: cell.column.columnDef.align,
+                        dataType: cell.column.columnDef.dataType,
+                      })}
+                      className={`${
+                        cell.column.id ? `cell-${cell.column.id}` : ``
+                      } ${cell.column.columnDef.className || ""}`
+                        .replace(/\s\s/, " ")
+                        .trimEnd()}
+                      style={{
+                        width: cell.column.columnDef.width,
+                        maxWidth: cell.column.columnDef.maxWidth,
+                        minWidth: cell.column.columnDef.minWidth,
                       }}
-                      cellContent={flexRender(
-                        cell.column.columnDef.cell,
-                        getFormattedValueContext(),
+                    >
+                      {cell.column.columnDef.tooltip ? (
+                        <TooltipWrapper
+                          tooltipOptions={{
+                            children: renderTooltipContent(cell),
+                            ...cell.column.columnDef?.tooltipOptions,
+                          }}
+                          cellContent={flexRender(
+                            cell.column.columnDef.cell,
+                            getFormattedValueContext(),
+                          )}
+                        ></TooltipWrapper>
+                      ) : (
+                        flexRender(
+                          cell.column.columnDef.cell,
+                          getFormattedValueContext(),
+                        )
                       )}
-                    ></TooltipWrapper>
-                  ) : (
-                    flexRender(
-                      cell.column.columnDef.cell,
-                      getFormattedValueContext(),
-                    )
-                  )}
-                </TableCell>
-              );
-            })}
-          </TableRow>
-        ))
-      ) : (
-        <TableRow>
-          <TableCell colSpan={parsedColumnsLength}>
-            {emptyTableMessage}
-          </TableCell>
-        </TableRow>
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={parsedColumnsLength}>
+                {emptyTableMessage}
+              </TableCell>
+            </TableRow>
+          )}
+        </>
       )}
     </TTableBody>
   );
