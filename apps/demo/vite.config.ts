ace5ecaa6af9c9602f9202ec09d17612e0700ca3
Suvash Vishowkarma <suvash@dzangolab.com>
2025-06-18 14:57:15 +0545

    fix(form/password): update error message style

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
   return {
-    base: "/react/",
     plugins: [
       react(),
       createHtmlPlugin({
