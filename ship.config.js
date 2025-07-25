51b21c4e11dd5e57be6df860590529b31b50a193
Suvash Vishowkarma <suvash@dzangolab.com>
2025-07-25 11:08:23 +0545

    chore: update shipjs config

--- ship.config.js
+++ ship.config.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   monorepo: {
     mainVersionFile: "package.json",
-    packagesToBump: ["packages/*", "tools/*"],
+    packagesToBump: ["packages/*"],
     packagesToPublish: ["packages/*"],
   },
   publishCommand: ({ isYarn, tag, defaultCommand, dir }) => {
